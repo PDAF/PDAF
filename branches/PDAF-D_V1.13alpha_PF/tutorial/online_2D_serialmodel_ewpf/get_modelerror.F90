1b66473964ec6d2322a3e6164f87714e849ef029
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2018-02-19 09:23:31 +0000

    Adding copy of tutorial test case adapted for EWPF.

---
+++ branches/PDAF-D_V1.13alpha_PF/tutorial/online_2D_serialmodel_ewpf/get_modelerror.F90
@@ -0,0 +1,55 @@
+!$Id: obs_op_pdaf.F90 3 2013-09-05 10:28:51Z lnerger $
+!BOP
+!
+! !ROUTINE: get_modelerror --- initialize random model error
+!
+! !INTERFACE:
+SUBROUTINE get_modelerror(dim_state, modelerror, rnd_type)
+
+! !DESCRIPTION:
+! User-supplied routine for PDAF.
+! Get a single realization of modelerror.
+!
+! The routine is called by all filter processes.
+!
+! !REVISION HISTORY:
+! 2014-05 - Paul Kirchgessner
+! Later revisions - see svn log
+
+  USE mod_parallel_pdaf, & 
+       ONLY: mype_world
+
+  IMPLICIT NONE
+
+! Arguments
+  INTEGER, INTENT(in) :: dim_state
+  REAL,INTENT(out)    :: modelerror(dim_state)
+  INTEGER, INTENT(in) :: rnd_type   ! 0 - normalrandom numbers
+                                    ! 1 - uniformrandom numbers
+
+! Local variables
+  INTEGER  :: i, j, k
+  INTEGER, SAVE :: seed(4)
+  INTEGER, SAVE :: firsttime = 1
+
+
+! Initialize seed
+  IF (firsttime==1) THEN
+     seed(1) = 1
+     seed(2) = 2
+     seed(3) = 7+mype_world
+     seed(4) = 17
+
+     firsttime = 0
+  ENDIF
+
+! Initialize error
+  IF (rnd_type==0) THEN
+     CALL dlarnv(3,seed,dim_state,modelerror)
+  ELSEIF (rnd_type==1) THEN
+     CALL dlarnv(2,seed,dim_state,modelerror)
+  END IF
+
+END SUBROUTINE get_modelerror
+ 
+
