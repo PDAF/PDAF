3a3b634d3d409ecf28e64726d1bd8c5905f4974d
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2019-05-29 08:42:38 +0000

    Adding rotated ensmeble files.

--- branches/spp1788/tutorial/inputs_online/pdaf_tutorial_online.m
+++ branches/spp1788/tutorial/inputs_online/pdaf_tutorial_online.m
@@ -64,6 +64,34 @@ cb=colorbar
 set(cb,'fontsize',16)
 title('Initial estimate (ensemble mean)','fontsize',18)
 
+
+% Ensemble states - inverted
+for k=1:dim_ens
+    for j=1:dim_x
+        for i=1:dim_y
+            ensB(i,j,k) = sin(2*pi*(i/18-j/36)+2*0.5*pi*(k+5)/dim_ens);
+        end
+    end
+    figure
+    field_plot=zeros(dim_y+1, dim_x+1);
+    field_plot(1:dim_y,1:dim_x) = ensB(:,:,k);
+    pcolor(field_plot)
+    set(gca,'fontsize',16)
+    cb=colorbar
+    set(cb,'fontsize',16)
+    title(['B-Ensemble member ' num2str(k)],'fontsize',18)
+end
+
+figure
+field_plot=zeros(dim_y+1, dim_x+1);
+ensmeanB = mean(ensB,3);
+field_plot(1:dim_y,1:dim_x) = ensmeanB;
+pcolor(field_plot)
+set(gca,'fontsize',16)
+cb=colorbar
+set(cb,'fontsize',16)
+title('B: Initial estimate (ensemble mean)','fontsize',18)
+
 % Observations
 obs_error = stddev_obs * randn(dim_y, dim_x, dim_step+1);
 full_obs = field + obs_error;
@@ -76,7 +104,7 @@ for step=1:dim_step+1
     set(gca,'fontsize',16)
     cb=colorbar
     set(cb,'fontsize',16)
-    title(['Disturbed true state, step ' num2str(step-1)],'fontsize',18)
+    title(['Perturbed true state, step ' num2str(step-1)],'fontsize',18)
 end
 
 obs = zeros(dim_y, dim_x, dim_step+1)-999;
@@ -134,6 +162,16 @@ for k=1:dim_ens
     fid = fopen(['ens_' num2str(k) '.txt'],'w');
     for i=1:dim_y
         fprintf(fid,'%14.8f',ens(i,:,k));
+        fprintf(fid,'\n')
+    end
+    fclose(fid)
+end
+
+% Ensemble
+for k=1:dim_ens
+    fid = fopen(['ensB_' num2str(k) '.txt'],'w');
+    for i=1:dim_y
+        fprintf(fid,'%14.8f',ensB(i,:,k));
         fprintf(fid,'\n')
     end
     fclose(fid)
