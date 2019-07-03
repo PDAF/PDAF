7bb368fe16a152487c4610e930cb1e68a4bcb16e
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2019-07-03 13:52:49 +0000

    Revised rungenobs.sh script for Lorenz96

--- branches/PDAF_1.14a_PF/testsuite/src/lorenz96/tools/rungenobs.sh
+++ branches/PDAF_1.14a_PF/testsuite/src/lorenz96/tools/rungenobs.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-# This script is an exam[le showing how one can generate synthetic observations
-# with PDAF. 
+# This script is an example showing how we can generate
+# synthetic observations with PDAF.
+#
+# We run with ensemble size 2 here to avoid that NaNs
+# appear in the output. However, running with dim_ens=1
+# also works and is recommended.
 #
 # 2019-02, Lars Nerger, AWI
 # $Id$
@@ -9,9 +13,9 @@
 EXE="./pdaf_lorenz_96"
 
 # General settings for all experiments
-DEFAULTS="-total_steps 2000 -step_null 1000 -dim_ens 1 -type_ensinit 'tru'"
+DEFAULTS="-total_steps 2000 -step_null 1000 -dim_ens 2 -type_ensinit 'tru'"
 
 # Run experiments
 FORGET=1 
 $EXE $DEFAULTS -filtertype 11 -forget $FORGET \
-	-file_asml t11_N1_genobs.nc
+	-file_asml t11_N2_genobs.nc
