383e784d76514d183d6ac77480cad15c84b61bfa
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2019-07-03 14:52:12 +0000

    Adding PF.

---
+++ trunk/PDAF_trunk/src/PDAF-D_pf_memtime.F90
@@ -0,0 +1,163 @@
+! Copyright (c) 2004-2018 Lars Nerger
+!
+! This file is part of PDAF.
+!
+! PDAF is free software: you can redistribute it and/or modify
+! it under the terms of the GNU Lesser General Public License
+! as published by the Free Software Foundation, either version
+! 3 of the License, or (at your option) any later version.
+!
+! PDAF is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU Lesser General Public License for more details.
+!
+! You should have received a copy of the GNU Lesser General Public
+! License along with PDAF.  If not, see <http://www.gnu.org/licenses/>.
+!
+!$Id: PDAF-D_pf_memtime.F90 89 2019-03-08 15:07:40Z lnerger $
+!BOP
+!
+! !ROUTINE: PDAF_pf_memtime --- Display timing and memory information for PF
+!
+! !INTERFACE:
+SUBROUTINE PDAF_pf_memtime(printtype)
+
+! !DESCRIPTION:
+! This routine displays the PDAF-internal timing and
+! memory information for the particle filter.
+!
+! !  This is a core routine of PDAF and
+!    should not be changed by the user   !
+!
+! !REVISION HISTORY:
+! 2019-05 - Lars Nerger - Initial code
+! Later revisions - see svn log
+!
+! !USES:
+  USE PDAF_timer, &
+       ONLY: PDAF_time_tot
+  USE PDAF_memcounting, &
+       ONLY: PDAF_memcount_get
+  USE PDAF_mod_filter, &
+       ONLY: subtype_filter
+  USE PDAF_mod_filtermpi, &
+       ONLY: filterpe
+
+  IMPLICIT NONE
+
+! !ARGUMENTS:
+  INTEGER, INTENT(in) :: printtype    ! Type of screen output:  
+                                      ! (1) timings, (2) memory
+!EOP
+
+! *** Local variables ***
+  INTEGER :: i   ! Counter
+
+
+! ********************************
+! *** Print screen information ***
+! ********************************
+
+  ptype: IF (printtype == 1) THEN
+
+! **************************************
+! *** Print basic timing information ***
+! **************************************
+
+     ! Generic part
+     WRITE (*, '(//a, 21x, a)') 'PDAF', 'PDAF Timing information'
+     WRITE (*, '(a, 10x, 45a)') 'PDAF', ('-', i=1, 45)
+     WRITE (*, '(a, 12x, a, F11.3, 1x, a)') &
+          'PDAF', 'Generate state ensemble:', pdaf_time_tot(1), 's'
+     IF (subtype_filter /= 5) THEN
+        WRITE (*, '(a, 18x, a, F11.3, 1x, a)') 'PDAF', 'Time of forecasts:', pdaf_time_tot(2), 's'
+     END IF
+
+     IF (filterpe) THEN
+        ! Filter-specific part
+        WRITE (*, '(a, 14x, a, F11.3, 1x, a)') 'PDAF', 'Time for assimilation:', pdaf_time_tot(3), 's'
+
+       ! Generic part B
+        WRITE (*, '(a, 16x, a, F11.3, 1x, a)') 'PDAF', 'Time of prepoststep:', pdaf_time_tot(5), 's'
+     END IF
+
+  ELSE IF (printtype == 2) THEN ptype
+
+! *******************************
+! *** Print allocated memory  ***
+! *******************************
+
+     WRITE (*, '(/a, 23x, a)') 'PDAF', 'PDAF Memory overview'
+     WRITE (*, '(a, 10x, 45a)') 'PDAF', ('-', i=1, 45)
+     WRITE (*, '(a, 21x, a)') 'PDAF', 'Allocated memory  (MiB)'
+     WRITE (*, '(a, 14x, a, 1x, f10.3, a)') &
+          'PDAF', 'state and A:', pdaf_memcount_get(1, 'M'), ' MiB (persistent)'
+     WRITE (*, '(a, 11x, a, 1x, f10.3, a)') &
+          'PDAF', 'ensemble array:', pdaf_memcount_get(2, 'M'), ' MiB (persistent)'
+     WRITE (*, '(a, 12x, a, 1x, f10.3, a)') &
+          'PDAF', 'analysis step:', pdaf_memcount_get(3, 'M'), ' MiB (temporary)'
+
+  ELSE IF (printtype == 3) THEN ptype
+
+! *********************************************
+! *** Print second-level timing information ***
+! *********************************************
+
+     ! Generic part
+     WRITE (*, '(//a, 21x, a)') 'PDAF', 'PDAF Timing information'
+     WRITE (*, '(a, 10x, 45a)') 'PDAF', ('-', i=1, 45)
+     WRITE (*, '(a, 13x, a, F11.3, 1x, a)') &
+          'PDAF', 'Generate state ensemble (1):', pdaf_time_tot(1), 's'
+     IF (subtype_filter /= 5) THEN
+        WRITE (*, '(a, 19x, a, F11.3, 1x, a)') 'PDAF', 'Time of forecasts (2):', pdaf_time_tot(2), 's'
+        WRITE (*, '(a, 7x, a, F11.3, 1x, a)') 'PDAF', 'Time to collect/distribute ens (19):', pdaf_time_tot(19), 's'
+        IF (.not.filterpe) WRITE (*, '(a, 7x, a)') 'PDAF', &
+             'Note: for filterpe=F, the time (2) includes the wait time for the analysis step'
+     END IF
+
+     IF (filterpe) THEN
+        ! Filter-specific part
+        WRITE (*, '(a, 15x, a, F11.3, 1x, a)') 'PDAF', 'Time for assimilation (3):', pdaf_time_tot(3), 's'
+        WRITE (*, '(a, 11x, a, F11.3, 1x, a)') 'PDAF', 'init observation dimension (15):', pdaf_time_tot(15), 's'
+        WRITE (*, '(a, 15x, a, F11.3, 1x, a)') 'PDAF', 'compute filter weights (12):', pdaf_time_tot(12), 's'
+        WRITE (*, '(a, 19x, a, F11.3, 1x, a)') 'PDAF', 'perform resampling (10):', pdaf_time_tot(10), 's'
+
+        ! Generic part B
+        WRITE (*, '(a, 17x, a, F11.3, 1x, a)') 'PDAF', 'Time of prepoststep (5):', pdaf_time_tot(5), 's'
+     END IF
+
+  ELSE IF (printtype == 4) THEN ptype
+
+! *****************************************
+! *** Print detailed timing information ***
+! *****************************************
+
+     ! Generic part
+     WRITE (*, '(//a, 21x, a)') 'PDAF', 'PDAF Timing information'
+     WRITE (*, '(a, 10x, 45a)') 'PDAF', ('-', i=1, 45)
+     WRITE (*, '(a, 13x, a, F11.3, 1x, a)') &
+          'PDAF', 'Generate state ensemble (1):', pdaf_time_tot(1), 's'
+     IF (subtype_filter /= 5) THEN
+        WRITE (*, '(a, 19x, a, F11.3, 1x, a)') 'PDAF', 'Time of forecasts (2):', pdaf_time_tot(2), 's'
+        WRITE (*, '(a, 7x, a, F11.3, 1x, a)') 'PDAF', 'Time to collect/distribute ens (19):', pdaf_time_tot(19), 's'
+        IF (.not.filterpe) WRITE (*, '(a, 7x, a)') 'PDAF', &
+             'Note: for filterpe=F, the time (2) includes the wait time for the analysis step'
+     END IF
+
+     IF (filterpe) THEN
+        ! Filter-specific part
+        WRITE (*, '(a, 15x, a, F11.3, 1x, a)') 'PDAF', 'Time for assimilation (3):', pdaf_time_tot(3), 's'
+        WRITE (*, '(a, 11x, a, F11.3, 1x, a)') 'PDAF', 'init observation dimension (15):', pdaf_time_tot(15), 's'
+        WRITE (*, '(a, 15x, a, F11.3, 1x, a)') 'PDAF', 'compute filter weights (12):', pdaf_time_tot(12), 's'
+        WRITE (*, '(a, 19x, a, F11.3, 1x, a)') 'PDAF', 'perform resampling (10):', pdaf_time_tot(10), 's'
+        WRITE (*, '(a, 15x, a, F11.3, 1x, a)') 'PDAF', 'get resampling indices (21):', pdaf_time_tot(21), 's'
+        WRITE (*, '(a, 21x, a, F11.3, 1x, a)') 'PDAF', 'resample ensemble (22):', pdaf_time_tot(22), 's'
+
+        ! Generic part B
+        WRITE (*, '(a, 17x, a, F11.3, 1x, a)') 'PDAF', 'Time of prepoststep (5):', pdaf_time_tot(5), 's'
+     END IF
+  END IF ptype
+
+
+END SUBROUTINE PDAF_pf_memtime
