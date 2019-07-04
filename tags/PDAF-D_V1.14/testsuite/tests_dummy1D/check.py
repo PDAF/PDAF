cec93f7f4584906f602d220dcad2cadfa250d9d1
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2019-07-04 07:02:53 +0000

    Copy trunk for release V1.14

---
+++ tags/PDAF-D_V1.14/testsuite/tests_dummy1D/check.py
@@ -0,0 +1,25 @@
+#!/use/bin/env python2
+
+import numpy as np
+import argparse as ap
+
+if __name__ == "__main__":
+   parser = ap.ArgumentParser()
+   parser.add_argument('filename')
+   parser.add_argument('refdir')
+   args = parser.parse_args()
+
+   fname = args.filename
+   field = np.loadtxt(fname)
+   field_ref = np.loadtxt(args.refdir+fname) 		
+
+   #   diff = field_ref[len(field)-1,1]-field[len(field)-1,1]
+   diff1 = field_ref[:,1]-field[:,1]
+   diff2 = field_ref[:,1]-field[:,1]
+
+   limit=1.e-9
+
+   if (max(abs(diff1))<limit and max(abs(diff2))<limit):
+      print max(abs(diff1)), max(abs(diff2)), fname
+   else:
+      print '---> ', max(abs(diff1)), max(abs(diff2)), fname, 'WARNING!'
