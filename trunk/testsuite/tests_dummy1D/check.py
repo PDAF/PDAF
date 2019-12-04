ba5e585d20bcdfcdf85b14fb3960d75bef734ca0
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2019-12-04 16:04:44 +0000

    Correcting check.py (it tested the estimated error twice, and skipped the true ones)

--- trunk/testsuite/tests_dummy1D/check.py
+++ trunk/testsuite/tests_dummy1D/check.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
    #   diff = field_ref[len(field)-1,1]-field[len(field)-1,1]
    diff1 = field_ref[:,1]-field[:,1]
-   diff2 = field_ref[:,1]-field[:,1]
+   diff2 = field_ref[:,2]-field[:,2]
 
    limit=1.e-9
 
