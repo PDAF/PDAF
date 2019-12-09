074426e917ad843e183fb95babfd1ff1d282e6f1
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2019-12-09 18:03:02 +0000

    Adding Python checking to runtests.sh

--- trunk/tutorial/verification/check_offline.py
+++ trunk/tutorial/verification/check_offline.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
    for i in range (0,10):
       if i==0:
-	 fname = 'state_ana.txt'
+         fname = 'state_ana.txt'
       else:
          fname = 'ens_0'+str(i)+'_ana.txt'
       field = np.loadtxt(args.refdir+'/'+fname)
