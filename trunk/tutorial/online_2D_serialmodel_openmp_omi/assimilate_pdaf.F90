3cf563ee352c9cf1bd0b1f7bf6a7dee3b83c4bda
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2020-01-22 08:34:06 +0000

    Move most checks whether a certain observation is to be assimilated into the obs_TYPE_pdafomi module s

--- trunk/tutorial/online_2D_serialmodel_openmp_omi/assimilate_pdaf.F90
+++ trunk/tutorial/online_2D_serialmodel_openmp_omi/assimilate_pdaf.F90
@@ -24,25 +24,28 @@ SUBROUTINE assimilate_pdaf()
 
 
   ! External subroutines
-  EXTERNAL :: collect_state_pdaf, & ! Routine to collect a state vector from model fields
-       prepoststep_ens_pdaf, &      ! User supplied pre/poststep routine
-       prodRinvA_pdafomi, &            ! Provide product R^-1 A for some matrix A
-       init_obsvar_pdafomi, &          ! Initialize mean observation error variance
-       next_observation_pdaf, &     ! Provide time step, model time, &
-                                    ! and dimension of next observation
-       distribute_state_pdaf        ! Routine to distribute a state vector to model fields
-  EXTERNAL :: init_n_domains_pdaf, &   ! Provide number of local analysis domains
-       init_dim_l_pdaf, &              ! Initialize state dimension for local ana. domain
-       init_dim_obs_l_pdafomi,&           ! Initialize dim. of obs. vector for local ana. domain
-       g2l_state_pdaf, &               ! Get state on local ana. domain from global state
-       l2g_state_pdaf, &               ! Init global state from state on local analysis domain
-       g2l_obs_pdafomi, &                 ! Restrict a global obs. vector to local analysis domain
-       init_obs_l_pdafomi, &              ! Provide vector of measurements for local ana. domain
-       prodRinvA_l_pdafomi, &             ! Provide product R^-1 A for some local matrix A
-       init_obsvar_l_pdafomi, &           ! Initialize local mean observation error variance
-       init_obs_f_pdafomi, &              ! Provide full vector of measurements for PE-local domain
-       obs_op_f_pdafomi, &                ! Obs. operator for full obs. vector for PE-local domain
-       init_dim_obs_f_pdafomi             ! Get dimension of full obs. vector for PE-local domain
+  ! Interface between model and PDAF, and prepoststep
+  EXTERNAL :: collect_state_pdaf, &  ! Collect a state vector from model fields
+       distribute_state_pdaf, &      ! Distribute a state vector to model fields
+       next_observation_pdaf, &      ! Provide time step of next observation
+       prepoststep_ens_pdaf          ! User supplied pre/poststep routine
+  ! Localization of state vector
+  EXTERNAL :: init_n_domains_pdaf, & ! Provide number of local analysis domains
+       init_dim_l_pdaf, &            ! Initialize state dimension for local analysis domain
+       g2l_state_pdaf, &             ! Get state on local analysis domain from global state
+       l2g_state_pdaf                ! Update global state from state on local analysis domain
+  ! Interface to PDAF-OMI for local and global filters
+  EXTERNAL :: &
+       init_dim_obs_f_pdafomi, &     ! Get dimension of full obs. vector for PE-local domain
+       obs_op_f_pdafomi, &           ! Obs. operator for full obs. vector for PE-local domain
+       init_obs_f_pdafomi, &         ! Provide full vector of measurements for PE-local domain
+       init_dim_obs_l_pdafomi, &     ! Get dimension of obs. vector for local analysis domain
+       g2l_obs_pdafomi, &            ! Get local observation vector from global observation vector
+       init_obs_l_pdafomi, &         ! Provide vector of observations for local analysis domain
+       prodRinvA_l_pdafomi, &        ! Provide product R^-1 A for some local matrix A
+       init_obsvar_l_pdafomi, &      ! Initialize local mean observation error variance
+       init_obsvar_pdafomi, &        ! Initialize mean observation error variance
+       prodRinvA_pdafomi             ! Provide product R^-1 A for some matrix A for global filter
 
 
 ! *********************************
@@ -58,7 +61,8 @@ SUBROUTINE assimilate_pdaf()
           init_dim_obs_f_pdafomi, obs_op_f_pdafomi, init_obs_f_pdafomi, init_obs_l_pdafomi, &
           prepoststep_ens_pdaf, prodRinvA_l_pdafomi, init_n_domains_pdaf, &
           init_dim_l_pdaf, init_dim_obs_l_pdafomi, g2l_state_pdaf, l2g_state_pdaf, &
-          g2l_obs_pdafomi, init_obsvar_pdafomi, init_obsvar_l_pdafomi, next_observation_pdaf, status_pdaf)
+          g2l_obs_pdafomi, init_obsvar_pdafomi, init_obsvar_l_pdafomi, &
+          next_observation_pdaf, status_pdaf)
   END IF
 
   ! Check for errors during execution of PDAF
