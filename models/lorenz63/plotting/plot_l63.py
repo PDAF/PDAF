b615d9822422138758540efcef2082e030512a68
Lars Nerger <94826174+larsnerger@users.noreply.github.com>
2020-02-26 08:10:55 +0000

    Adding plot function plot_2state_series to plot e.g. true and assimilated state over time.

--- models/lorenz63/plotting/plot_l63.py
+++ models/lorenz63/plotting/plot_l63.py
@@ -310,6 +310,96 @@ def plot_state_series(filename, variable, choice='t'):
                   "").format(statestr, strvari)
                  )
 
+
+def plot_2state_series(filename1, filename2, variable, choice1='t', choice2='a'):
+    """
+    Open NetCDF output from the Lorenz63 model and plot two time
+    series of the selected variable.
+
+    Parameters
+    ----------
+    filename1 : str
+        file name including path
+    filename2 : str
+        file name including path
+    variable : int
+        variable to plot (1=x, 2=y, 3=z)
+    choice1 : str  (choice for file 1)
+        't' for true state, 'f' for forecast, 'a' for analysis,
+        'i' for initial state. Default is 't'.
+    choice2 : str  (choice for file 2)
+        't' for true state, 'f' for forecast, 'a' for analysis,
+        'i' for initial state. Default is 't'.
+
+    """
+
+    if choice1 not in 'tfai':
+        raise Exception(("{0} is not a valid choice. Valid choices are "
+                         "'t', 'f', 'a', 'i'.".format(choice1))
+                        )
+                        
+    if choice2 not in 'tfai':
+        raise Exception(("{0} is not a valid choice. Valid choices are "
+                         "'t', 'f', 'a', 'i'.".format(choice2))
+                        )
+
+    with nc.Dataset(filename1) as ncfile1:
+        print("File contains {0:d} time steps.".format(
+                ncfile1['step'].size
+                                                       )
+              )
+        time1 = ncfile1['time'][:]
+        step1 = ncfile1['step'][:]
+
+        if choice1 == 't':
+            state1    = ncfile1['state'][:,variable-1]
+            statestr1 = 'true state'
+        elif choice1 == 'i':
+            state1    = ncfile1['state_ini'][:,variable-1]
+            statestr1 = 'initial state'
+        elif choice1 == 'f':
+            state1    = ncfile1['state_for'][:,variable-1]
+            statestr1 = 'forecast estimate'
+        else:
+            state1    = ncfile1['state_ana'][:,variable-1]
+            statestr1 = 'analysis estimate'
+
+    with nc.Dataset(filename2) as ncfile2:
+        print("File contains {0:d} time steps.".format(
+                ncfile2['step'].size
+                                                       )
+              )
+        time2 = ncfile2['time'][:]
+        step2 = ncfile2['step'][:]
+
+        if choice2 == 't':
+            state2    = ncfile2['state'][:,variable-1]
+            statestr2 = 'true state'
+        elif choice2 == 'i':
+            state2    = ncfile2['state_ini'][:,variable-1]
+            statestr2 = 'initial state'
+        elif choice1 == 'f':
+            state2    = ncfile2['state_for'][:,variable-1]
+            statestr2 = 'forecast estimate'
+        else:
+            state2    = ncfile2['state_ana'][:,variable-1]
+            statestr2 = 'analysis estimate'
+
+    if variable==1:
+        strvari = 'X'
+    elif variable==2:
+        strvari = 'Y'
+    elif variable==3:
+        strvari = 'Z'
+
+    fig, ax = plt.subplots()
+    ax.plot(time1, state1, 'r',label=statestr1)
+    ax.plot(time2, state2, 'b',label=statestr2)
+    ax.legend()
+    ax.set_title(("Lorenz63 model, variable {0}"
+                  "").format(strvari)
+                 )
+
 def _parse_str(arg):
     """
     Convert string to integer or False, if possible.
