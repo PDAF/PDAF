901c620398b3e267f76d7e623937730c788ec886
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2020-05-22 05:47:06 +0000

    Creating a copy of current PDAF revision to keep OMIV1.0 (before revision of OMI interfaces)

---
+++ tags/PDAF-D_V1.15.1_OMI1.0/models/lorenz96/plotting/plot_obs.m
@@ -0,0 +1,54 @@
+function[] = plot_obs(varargin)
+% plot_obs('filename with path', timestep)
+%
+% Opens a NetCDF holding observations for the Lorenz96 model
+% and plots the observation at a selected time.
+%
+% Arguments:
+% 'filename with path': File name including path
+% iteration           : Time step in file to be shown
+%
+% This file is part of the test suite of PDAF.
+
+global obs;
+
+if length(varargin)<2
+  disp('Function arguments incomplete - see help!')
+  return
+end
+
+% Name of file holding state trajectory
+filename = varargin{1}
+
+% Iteration in file to be shown
+iter = varargin{2}-1
+
+% Open file
+if exist(filename,'file')
+  nc=netcdf.open(filename,'nowrite');
+  varid = netcdf.inqDimID(nc,'timesteps');
+  [varname, n_steps] = netcdf.inqDim(nc, varid);
+
+  disp(['file contains ',int2str(n_steps), ' timesteps'])
+else
+  disp('file does not exist!')
+end
+
+% Read state dimension
+varid = netcdf.inqDimID(nc,'dim_state');
+[varname dim] = netcdf.inqDim(nc,varid);
+
+% Read state
+varid = netcdf.inqVarID(nc,'obs');
+obs = netcdf.getVar(nc,varid,[0,iter],[dim,1]);
+varid = netcdf.inqVarID(nc,'time');
+time = netcdf.getVar(nc,varid,iter,1);
+varid = netcdf.inqVarID(nc,'step');
+step = netcdf.getVar(nc,varid,iter,1);
+
+netcdf.close(nc);
+
+% Plot state
+hf=figure;
+plot(obs,'b+-')
+title(['Observations for Lorenz96 model at time ',num2str(time),' (time step ',num2str(step),')'])
---
+++ tags/PDAF-D_V1.15.1_OMI1.0/tutorial/online_2D_parallelmodel_omi/mod_model.F90
@@ -0,0 +1,25 @@
+!$Id$
+!> Module for model-specific variables
+!!
+!! This module provides variables needed for the 
+!! 2-dimensional tutorial model without parallelization.
+!!
+!! __Revision history:__
+!! * 2013-09 - Lars Nerger - Initial code
+!! * Later revisions - see repository log
+!!
+MODULE mod_model
+
+  IMPLICIT NONE
+  SAVE
+
+! *** Variables specific for 2D tutorial model ***
+
+  INTEGER :: nx                     !< Size of 2D grid in x-direction
+  INTEGER :: ny                     !< Size of 2D grid in y-direction
+  INTEGER :: total_steps            !< Total number of time steps
+  REAL, ALLOCATABLE :: field_p(:,:) !< Process-local part of model field
+
+  INTEGER :: nx_p                 !< Process-local size in x-direction
+
+END MODULE mod_model
