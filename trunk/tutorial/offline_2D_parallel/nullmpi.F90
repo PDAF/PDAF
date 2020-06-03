2ef7859c6bf0d4a019127a10448f7ad5c8492724
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2020-06-03 11:22:02 +0000

    Adding MPI_Gather and MPI_GatherV to nullmpi.F90. Updaing all nullmpi.F90 files

--- trunk/tutorial/offline_2D_parallel/nullmpi.F90
+++ trunk/tutorial/offline_2D_parallel/nullmpi.F90
@@ -1,4 +1,4 @@
-!$Id: nullmpi.F90 1369 2013-04-24 16:38:17Z lnerger $
+!$Id: nullmpi.F90 1565 2015-02-28 17:04:41Z lnerger $
 !BOP
 !
 ! !ROUTINE: mpi_init() --- Pseudo-implementation of MPI_init
@@ -24,6 +24,35 @@ SUBROUTINE mpi_init(i)
   i = 0
 
 END SUBROUTINE mpi_init
+
+! ------------------------------------------------------------------------------
+!BOP
+!
+! !ROUTINE: mpi_initialized() --- Pseudo-implementation of MPI_initialized
+!
+! !INTERFACE:
+SUBROUTINE mpi_initialized(iniflag, i)
+
+! !DESCRIPTION:
+! This routine simulates MPI functionality for
+! a program running on a single processor. Its
+! purpose is to avoid the need of a real MPI
+! library when running serial jobs. 
+!
+! !REVISION HISTORY:
+! 2004-10 - Lars Nerger - Initial code
+! Later revisions - see svn log
+!EOP
+
+  IMPLICIT NONE
+
+  LOGICAL :: iniflag
+  INTEGER :: i
+  
+  iniflag = .false.
+  i = 0
+
+END SUBROUTINE mpi_initialized
 
 ! ------------------------------------------------------------------------------
 SUBROUTINE mpi_finalize(i)
@@ -125,6 +154,25 @@ SUBROUTINE MPI_Send(field, dim, datatype, pe_source, pe_target, &
 END SUBROUTINE MPI_Send
 
 ! ------------------------------------------------------------------------------
+SUBROUTINE MPI_Isend(field, dim, datatype, pe_source, pe_target, &
+     comm, reqs, mpierr)
+
+  IMPLICIT NONE
+
+  INTEGER :: dim
+  INTEGER :: field(dim)
+  INTEGER :: datatype
+  INTEGER :: pe_source
+  INTEGER :: pe_target
+  INTEGER :: comm
+  INTEGER :: reqs(dim)
+  INTEGER :: mpierr
+
+  mpierr = 0
+
+END SUBROUTINE MPI_Isend
+
+! ------------------------------------------------------------------------------
 SUBROUTINE MPI_Recv(field, dim, datatype, pe_source, pe_target, &
      comm, flag, mpierr)
 
@@ -142,6 +190,25 @@ SUBROUTINE MPI_Recv(field, dim, datatype, pe_source, pe_target, &
   mpierr = 0
 
 END SUBROUTINE MPI_Recv
+
+! ------------------------------------------------------------------------------
+SUBROUTINE MPI_IRecv(field, dim, datatype, pe_source, pe_target, &
+     comm, reqs, mpierr)
+
+  IMPLICIT NONE
+
+  INTEGER :: dim
+  INTEGER :: field(dim)
+  INTEGER :: datatype
+  INTEGER :: pe_source
+  INTEGER :: pe_target
+  INTEGER :: comm
+  INTEGER :: reqs(dim)
+  INTEGER :: mpierr
+
+  mpierr = 0
+
+END SUBROUTINE MPI_IRecv
 
 ! ------------------------------------------------------------------------------
 SUBROUTINE MPI_BCast(field, dim, datatype, pe_source, comm, mpierr)
@@ -239,3 +306,64 @@ SUBROUTINE MPI_AllGatherV(field_in, dim_in, type_in, field_out, dim_out, &
   mpierr = 0
 
 END SUBROUTINE MPI_AllGatherV
+
+! ------------------------------------------------------------------------------
+SUBROUTINE MPI_Gather(field_in, dim_in, type_in, &
+     field_out, dim_out, type_out, &
+     iroot, comm, mpierr)
+
+  IMPLICIT NONE
+
+  INTEGER :: dim_in
+  REAL    :: field_in(dim_in)
+  INTEGER :: type_in
+  INTEGER :: dim_out
+  REAL    :: field_out(dim_out)
+  INTEGER :: dis
+  INTEGER :: type_out
+  INTEGER :: comm
+  INTEGER :: iroot
+  INTEGER :: mpierr
+
+  field_out = field_in
+  mpierr = 0
+
+END SUBROUTINE MPI_Gather
+
+! ------------------------------------------------------------------------------
+SUBROUTINE MPI_GatherV(field_in, dim_in, type_in, &
+     field_out, dim_out, dis, type_out, &
+     iroot, comm, mpierr)
+
+  IMPLICIT NONE
+
+  INTEGER :: dim_in
+  REAL    :: field_in(dim_in)
+  INTEGER :: type_in
+  INTEGER :: dim_out
+  REAL    :: field_out(dim_out)
+  INTEGER :: dis
+  INTEGER :: type_out
+  INTEGER :: comm
+  INTEGER :: iroot
+  INTEGER :: mpierr
+
+  field_out = field_in
+  mpierr = 0
+
+END SUBROUTINE MPI_GatherV
+
+
+! ------------------------------------------------------------------------------
+SUBROUTINE MPI_Waitall(nreqs, reqs, stats, mpierr)
+
+  IMPLICIT NONE
+
+  INTEGER :: nreqs
+  INTEGER :: reqs(nreqs)
+  INTEGER :: stats(nreqs)
+  INTEGER :: mpierr
+
+  mpierr = 0
+
+END SUBROUTINE MPI_Waitall
