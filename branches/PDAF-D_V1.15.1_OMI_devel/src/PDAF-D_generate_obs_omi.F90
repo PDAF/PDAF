48f2a0cb1f50b3453bfd492d976670df2c52f838
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2020-06-07 10:07:43 +0000

    Adding PDAF_assimilate routines for OMI interface. Adding OMI? interface routines to inteface_module

--- branches/PDAF-D_V1.15.1_OMI_devel/src/PDAF-D_generate_obs_omi.F90
+++ branches/PDAF-D_V1.15.1_OMI_devel/src/PDAF-D_generate_obs_omi.F90
@@ -15,53 +15,56 @@
 ! You should have received a copy of the GNU Lesser General Public
 ! License along with PDAF.  If not, see <http://www.gnu.org/licenses/>.
 !
-!$Id: PDAF-D_put_state_generate_obs_omi.F90 374 2020-02-26 12:49:56Z lnerger $
+!$Id: PDAF-D_generate_obsomi.F90 374 2020-02-26 12:49:56Z lnerger $
 !BOP
 !
-! !ROUTINE: PDAF_put_state_generate_obs_omi --- Interface to transfer state to PDAF
+! !ROUTINE: PDAF_generate_obsomi --- Interface to transfer state to PDAF
 !
 ! !INTERFACE:
-SUBROUTINE PDAF_put_state_generate_obs_omi(collect_state_pdaf, init_dim_obs_f_pdaf, &
-     obs_op_f_pdaf, get_obs_f_pdaf, prepoststep_pdaf, outflag)
+SUBROUTINE PDAF_generate_obs_omi(collect_state_pdaf, distribute_state_pdaf, &
+       init_dim_obs_f_pdaf, obs_op_f_pdaf, get_obs_f_pdaf, prepoststep_pdaf, &
+       next_observation_pdaf, outflag)
 
 ! !DESCRIPTION:
-! Interface routine called from the model after the 
+! Interface routine called from the model during the 
 ! forecast of each ensemble state to transfer data
-! from the model to PDAF. 
+! from the model to PDAF and to perform the analysis
+! step.
 !
 ! This routine provides the simplified interface
 ! where names of user-provided subroutines are
 ! fixed. It simply calls the routine with the
 ! full interface using pre-defined routine names.
 !
-! Variant for observation generation with domain 
+! Variant for gnerating observations with domain
 ! decomposition.
 !
 ! !  This is a core routine of PDAF and
 !    should not be changed by the user   !
 !
 ! !REVISION HISTORY:
-! 2020-06 - Lars Nerger - Initial code
+! 2019-01 - Lars Nerger - Initial code
 ! Later revisions - see svn log
 !
 ! !USES:
   IMPLICIT NONE
-
+  
 ! !ARGUMENTS:
   INTEGER, INTENT(inout) :: outflag ! Status flag
   
 ! ! Names of external subroutines 
   EXTERNAL :: collect_state_pdaf, &  ! Routine to collect a state vector
-       prepoststep_pdaf              ! User supplied pre/poststep routine
+       distribute_state_pdaf, &      ! Routine to distribute a state vector
+       prepoststep_pdaf, &           ! User supplied pre/poststep routine
+       next_observation_pdaf         ! Provide time step, time and dimension of next observation
   EXTERNAL :: init_dim_obs_f_pdaf, & ! Initialize dimension of observation vector
        obs_op_f_pdaf, &              ! Observation operator
        get_obs_f_pdaf                ! Initialize observation vector
   EXTERNAL :: PDAFomi_init_obserr_f_cb ! Initialize mean observation error variance
 
-
 ! !CALLING SEQUENCE:
 ! Called by: model code  
-! Calls: PDAF_put_state_generate_obs
+! Calls: PDAF_generate_obs
 !EOP
 
 
@@ -69,7 +72,8 @@ SUBROUTINE PDAF_put_state_generate_obs_omi(collect_state_pdaf, init_dim_obs_f_pd
 ! *** Call the full put_state interface routine  ***
 ! **************************************************
 
-  CALL PDAF_put_state_generate_obs(collect_state_pdaf, init_dim_obs_f_pdaf, obs_op_f_pdaf, &
-       PDAFomi_init_obserr_f_cb, get_obs_f_pdaf, prepoststep_pdaf, outflag)
+  CALL PDAF_generate_obs(collect_state_pdaf, distribute_state_pdaf, &
+       init_dim_obs_f_pdaf, obs_op_f_pdaf, PDAFomi_init_obserr_f_cb, get_obs_f_pdaf, &
+       prepoststep_pdaf, next_observation_pdaf, outflag)
 
-END SUBROUTINE PDAF_put_state_generate_obs_omi
+END SUBROUTINE PDAF_generate_obs_omi
