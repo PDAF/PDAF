df333c7ab627b1b7d86ca0b9ddf2e5d5e9f899ef
Lars Nerger <94826174+larsnerger@users.noreply.github.com>
2020-06-10 13:28:05 +0000

    COrrect order of finalize_parallel and finalize_pdaf

--- tutorial/online_2D_parallelmodel/main_pdaf.F90
+++ tutorial/online_2D_parallelmodel/main_pdaf.F90
@@ -69,12 +69,11 @@ PROGRAM MAIN
 ! ***      Clean up      ***
 ! **************************
 
-  CALL finalize_parallel()
-
 #ifdef USE_PDAF
   ! End parallelization
   CALL finalize_pdaf()
 #endif
 
+  CALL finalize_parallel()
 
 END PROGRAM MAIN
