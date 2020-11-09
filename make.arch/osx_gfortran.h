c80aecf0e0400adc63c513b3ac2e278f08a566db
Lars Nerger <94826174+larsnerger@users.noreply.github.com>
2020-11-09 17:03:07 +0000

    Adding ' -fallow-argument-mismatch' for gcc-10

--- make.arch/osx_gfortran.h
+++ make.arch/osx_gfortran.h
@@ -33,7 +33,7 @@ CPP_DEFS = -DUSE_PDAF
 # To use OpenMP parallelization in PDAF, specify it here (-fopenmp (gfortran) or -openmp (ifort))
 #   (You should explicitly define double precision for floating point
 #   variables in the compilation)  
-OPT = -O3 -fdefault-real-8 -fopenmp
+OPT = -O3 -fdefault-real-8 -fopenmp -fallow-argument-mismatch
 
 # Optimization specifications for Linker
 OPT_LNK = $(OPT)
