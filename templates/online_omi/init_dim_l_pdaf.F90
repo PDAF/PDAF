3921ab72467031c9306f8f17d667b376cb9f5e31
Lars Nerger <94826174+larsnerger@users.noreply.github.com>
2020-11-23 06:43:52 +0000

    Copy current online template for online-OMI

---
+++ templates/online_omi/init_dim_l_pdaf.F90
@@ -0,0 +1,73 @@
+!$Id$
+!BOP
+!
+! !ROUTINE: init_dim_l_pdaf --- Set dimension of local model state
+!
+! !INTERFACE:
+SUBROUTINE init_dim_l_pdaf(step, domain_p, dim_l)
+
+! !DESCRIPTION:
+! User-supplied routine for PDAF.
+! Used in the filters: LSEIK/LETKF/LESTKF
+!
+! The routine is called during analysis step
+! in the loop over all local analysis domain.
+! It has to set the dimension of local model 
+! state on the current analysis domain.
+!
+! The routine is called by each filter process.
+!
+! !REVISION HISTORY:
+! 2005-09 - Lars Nerger - Initial code
+! Later revisions - see svn log
+!
+! !USES:
+  USE mod_assimilation, &      ! Variables for assimilation
+       ONLY: coords_l, id_lstate_in_pstate
+
+  IMPLICIT NONE
+
+! !ARGUMENTS:
+  INTEGER, INTENT(in)  :: step     ! Current time step
+  INTEGER, INTENT(in)  :: domain_p ! Current local analysis domain
+  INTEGER, INTENT(out) :: dim_l    ! Local state dimension
+
+! !CALLING SEQUENCE:
+! Called by: PDAF_lseik_update   (as U_init_dim_l)
+! Called by: PDAF_lestkf_update  (as U_init_dim_l)
+! Called by: PDAF_letkf_update   (as U_init_dim_l)
+! Called by: PDAF_lnetf_update   (as U_init_dim_l)
+!EOP
+
+
+! ****************************************
+! *** Initialize local state dimension ***
+! ****************************************
+
+  ! Template reminder - delete when implementing functionality
+  WRITE (*,*) 'TEMPLATE init_dim_l_pdaf.F90: Set local state dimension here!'
+
+!   dim_l = ??
+
+
+! **********************************************
+! *** Initialize coordinates of local domain ***
+! **********************************************
+
+  ! Global coordinates of local analysis domain
+
+!  coords_l = ??
+
+
+! ******************************************************
+! *** Initialize array of indices of the local state ***
+! ***  vector elements in the global state vector.   ***
+! ******************************************************
+
+  ! Allocate array
+  IF (ALLOCATED(id_lstate_in_pstate)) DEALLOCATE(id_lstate_in_pstate)
+  ALLOCATE(id_lstate_in_pstate(dim_l))
+
+!  id_lstate_in_pstate = ??
+
+END SUBROUTINE init_dim_l_pdaf
