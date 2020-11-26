3ef82b6ace2969f98b4d9332938c3a154309bd52
Lars Nerger <94826174+larsnerger@users.noreply.github.com>
2020-11-26 16:18:42 +0000

    Renaming OMI tutorial cases removing _OMI

--- tutorial/runtests_omi.sh
+++ tutorial/runtests_omi.sh
@@ -11,30 +11,34 @@ echo "------------------ COMPILING ----------------"
 setenv PDAF_ARCH $ARCH
 echo -------------- PDAF_ARCH: $PDAF_ARCH
 
-echo "------------ online_2D_serialmodel_omi ---------------"
+echo "------------ offline_2D_serial --------------"
+cd offline_2D_serial
+make cleanall
+make
+cd ..
+
+echo "------------ offline_2D_parallel ---------------"
 setenv PDAF_ARCH $ARCH_MPI
 echo PDAF_ARCH: $PDAF_ARCH
-cd online_2D_serialmodel_omi
+cd offline_2D_parallel
+make cleanall
+make
+cd ..
+
+echo "------------ online_2D_serialmodel ---------------"
+setenv PDAF_ARCH $ARCH_MPI
+echo PDAF_ARCH: $PDAF_ARCH
+cd online_2D_serialmodel
 make clean
 make cleandata
 make model
 make model_pdaf
 cd ..
 
-echo "------------ online_2D_serialmodel_openmp_omi ---------------"
+echo "------------ online_2D_parallelmodel ---------------"
 setenv PDAF_ARCH $ARCH_MPI
 echo PDAF_ARCH: $PDAF_ARCH
-cd online_2D_serialmodel_openmp_omi
-make clean
-make cleandata
-make model
-make model_pdaf
-cd ..
-
-echo "------------ online_2D_parallelmodel_omi ---------------"
-setenv PDAF_ARCH $ARCH_MPI
-echo PDAF_ARCH: $PDAF_ARCH
-cd online_2D_parallelmodel_omi
+cd online_2D_parallelmodel
 make clean
 make cleandata
 make model
@@ -46,43 +50,56 @@ echo  " "
 echo "-------------------- RUNNING ----------------"
 
 
-echo "------------ online_2D_serialmodel_omi LESTKF ---------------"
+echo "------------ offline_2D_serial --------------"
 setenv OMP_NUM_THREADS 1
-cd online_2D_serialmodel_omi
-make cleandata
-mpirun --oversubscribe -np 9 ./model_pdaf -dim_ens 9 $DA_SPECS > ../out.online_2D_serialmodel_omi
+cd offline_2D_serial
+./PDAF_offline $DA_SPECS > ../out.offline_2D_serial
 cd ..
-python verification/check_online2.py online_2D_serialmodel_omi online_2D_serialmodel
+python verification/check_offline.py offline_2D_serial
 
-echo "------------ online_2D_serialmodel_openmp_omi LESTKF ---------------"
-setenv OMP_NUM_THREADS 2
-cd online_2D_serialmodel_openmp_omi
-make cleandata
-mpirun --oversubscribe -np 9 ./model_pdaf -dim_ens 9 $DA_SPECS > ../out.online_2D_serialmodel_openmp_omi
+echo "------------ offline_2D_openmp ---------------"
+setenv OMP_NUM_THREADS 4
+cd offline_2D_serial
+./PDAF_offline $DA_SPECS > ../out.offline_2D_openmp
 cd ..
-python verification/check_online2.py online_2D_serialmodel_openmp_omi online_2D_serialmodel_openmp
+python verification/check_offline.py offline_2D_serial
 
-echo "------------ online_2D_parallelmodel_omi LESTKF ---------------"
+echo "------------ offline_2D_parallel ---------------"
 setenv OMP_NUM_THREADS 1
-cd online_2D_parallelmodel_omi
-make cleandata
-mpirun --oversubscribe -np 18 ./model_pdaf -dim_ens 9 $DA_SPECS > ../out.online_2D_parallelmodel_omi
+cd offline_2D_parallel
+mpirun -np 4 ./PDAF_offline $DA_SPECS > ../out.offline_2D_parallel
 cd ..
-python verification/check_online2.py online_2D_parallelmodel_omi online_2D_parallelmodel
+python verification/check_offline.py offline_2D_parallel
 
-
-echo "------------ online_2D_serialmodel_omi ESTKF ---------------"
+echo "------------ online_2D_serialmodel LESTKF ---------------"
 setenv OMP_NUM_THREADS 1
-cd online_2D_serialmodel_omi
+cd online_2D_serialmodel
 make cleandata
-mpirun --oversubscribe -np 9 ./model_pdaf -dim_ens 9 $DA_SPECS2 > ../out.online_2D_serialmodel_omi_ESTKF
+mpirun --oversubscribe -np 9 ./model_pdaf -dim_ens 9 $DA_SPECS > ../out.online_2D_serialmodel
 cd ..
-python verification/check_online2.py online_2D_serialmodel_omi online_2D_serialmodel_ESTKF
+python verification/check_online2.py online_2D_serialmodel online_2D_serialmodel
 
-echo "------------ online_2D_parallelmodel_omi ESTKF ---------------"
+echo "------------ online_2D_parallelmodel LESTKF ---------------"
 setenv OMP_NUM_THREADS 1
-cd online_2D_parallelmodel_omi
+cd online_2D_parallelmodel
 make cleandata
-mpirun --oversubscribe -np 18 ./model_pdaf -dim_ens 9 $DA_SPECS2 > ../out.online_2D_parallelmodel_omi_ESTKF
+mpirun --oversubscribe -np 18 ./model_pdaf -dim_ens 9 $DA_SPECS > ../out.online_2D_parallelmodel
 cd ..
-python verification/check_online2.py online_2D_parallelmodel_omi online_2D_parallelmodel_ESTKF
+python verification/check_online2.py online_2D_parallelmodel online_2D_parallelmodel
+
+
+echo "------------ online_2D_serialmodel ESTKF ---------------"
+setenv OMP_NUM_THREADS 1
+cd online_2D_serialmodel
+make cleandata
+mpirun --oversubscribe -np 9 ./model_pdaf -dim_ens 9 $DA_SPECS2 > ../out.online_2D_serialmodel_ESTKF
+cd ..
+python verification/check_online2.py online_2D_serialmodel online_2D_serialmodel_ESTKF
+
+echo "------------ online_2D_parallelmodel ESTKF ---------------"
+setenv OMP_NUM_THREADS 1
+cd online_2D_parallelmodel
+make cleandata
+mpirun --oversubscribe -np 18 ./model_pdaf -dim_ens 9 $DA_SPECS2 > ../out.online_2D_parallelmodel_ESTKF
+cd ..
+python verification/check_online2.py online_2D_parallelmodel online_2D_parallelmodel_ESTKF
