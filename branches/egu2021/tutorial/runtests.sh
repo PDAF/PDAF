71fd7596e6edd887921677aaf4966eee408e11fb
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2021-03-29 14:28:59 +0000

    Revision of README for inclusion of two tutorials.

--- branches/egu2021/tutorial/runtests.sh
+++ branches/egu2021/tutorial/runtests.sh
@@ -1,8 +1,8 @@
 #!/bin/tcsh
 
 # ARCH and ARCH_MPI specify PDAF_ARCH without and with PDAF
-setenv ARCH osx_gfortran_openmpi #linux_gfortran
-setenv ARCH_MPI osx_gfortran_openmpi
+setenv ARCH linux_gfortran
+setenv ARCH_MPI linux_gfortran_openmpi
 setenv DA_SPECS "-filtertype 7"
 setenv DA_SPECS2 "-filtertype 6"
 setenv DA_SPECS3 "-filtertype 7 -assim_A .false. -assim_B .true"
@@ -11,20 +11,6 @@ echo "------------------ COMPILING ----------------"
 
 setenv PDAF_ARCH $ARCH
 echo -------------- PDAF_ARCH: $PDAF_ARCH
-
-echo "------------ offline_2D_serial --------------"
-cd offline_2D_serial
-make cleanall
-make
-cd ..
-
-echo "------------ offline_2D_parallel ---------------"
-setenv PDAF_ARCH $ARCH_MPI
-echo PDAF_ARCH: $PDAF_ARCH
-cd offline_2D_parallel
-make cleanall
-make
-cd ..
 
 echo "------------ online_2D_serialmodel ---------------"
 setenv PDAF_ARCH $ARCH_MPI
@@ -36,69 +22,9 @@ make model
 make model_pdaf
 cd ..
 
-echo "------------ online_2D_serialmodel_2fields ---------------"
-setenv PDAF_ARCH $ARCH_MPI
-echo PDAF_ARCH: $PDAF_ARCH
-cd online_2D_serialmodel_2fields
-make clean
-make cleandata
-make model
-make model_pdaf
-cd ..
-
-echo "------------ online_2D_parallelmodel ---------------"
-setenv PDAF_ARCH $ARCH_MPI
-echo PDAF_ARCH: $PDAF_ARCH
-cd online_2D_parallelmodel
-make clean
-make cleandata
-make model
-make model_pdaf
-cd ..
-
-echo "------------ online_2D_parallelmodel_fullpar ---------------"
-setenv PDAF_ARCH $ARCH_MPI
-echo PDAF_ARCH: $PDAF_ARCH
-cd online_2D_parallelmodel_fullpar
-make clean
-make cleandata
-make model_pdaf
-cd ..
-
-echo "------------ online_2D_parallelmodel_fullpar_1fpe ---------------"
-setenv PDAF_ARCH $ARCH_MPI
-echo PDAF_ARCH: $PDAF_ARCH
-cd online_2D_parallelmodel_fullpar_1fpe
-make clean
-make cleandata
-make model_pdaf
-cd ..
-
-
 echo  " "
 echo "-------------------- RUNNING ----------------"
 
-
-echo "------------ offline_2D_serial --------------"
-setenv OMP_NUM_THREADS 1
-cd offline_2D_serial
-./PDAF_offline $DA_SPECS > ../out.offline_2D_serial
-cd ..
-python verification/check_offline.py offline_2D_serial
-
-echo "------------ offline_2D_openmp ---------------"
-setenv OMP_NUM_THREADS 4
-cd offline_2D_serial
-./PDAF_offline $DA_SPECS > ../out.offline_2D_openmp
-cd ..
-python verification/check_offline.py offline_2D_serial
-
-echo "------------ offline_2D_parallel ---------------"
-setenv OMP_NUM_THREADS 1
-cd offline_2D_parallel
-mpirun -np 4 ./PDAF_offline $DA_SPECS > ../out.offline_2D_parallel
-cd ..
-python verification/check_offline.py offline_2D_parallel
 
 echo "------------ online_2D_serialmodel LESTKF ---------------"
 setenv OMP_NUM_THREADS 1
@@ -116,30 +42,6 @@ mpirun --oversubscribe -np 9 ./model_pdaf -dim_ens 9 $DA_SPECS > ../out.online_2
 cd ..
 python verification/check_online.py online_2D_serialmodel
 
-echo "------------ online_2D_serialmodel_2fields LESTKF ---------------"
-setenv OMP_NUM_THREADS 1
-cd online_2D_serialmodel_2fields
-make cleandata
-mpirun --oversubscribe -np 9 ./model_pdaf -dim_ens 9 $DA_SPECS > ../out.online_2D_serialmodel_2fields
-cd ..
-python verification/check_online2.py online_2D_serialmodel_2fields online_2D_serialmodel
-
-echo "------------ online_2D_serialmodel_2fields LESTKF obs-type B ---------------"
-setenv OMP_NUM_THREADS 1
-cd online_2D_serialmodel_2fields
-make cleandata
-mpirun --oversubscribe -np 9 ./model_pdaf -dim_ens 9 $DA_SPECS3 > ../out.online_2D_serialmodel_2fields_obsB
-cd ..
-python verification/check_online3.py online_2D_serialmodel_2fields online_2D_serialmodel_2fields_obsB
-
-echo "------------ online_2D_parallelmodel LESTKF ---------------"
-setenv OMP_NUM_THREADS 1
-cd online_2D_parallelmodel
-make cleandata
-mpirun --oversubscribe -np 18 ./model_pdaf -dim_ens 9 $DA_SPECS > ../out.online_2D_parallelmodel
-cd ..
-python verification/check_online.py online_2D_parallelmodel
-
 echo "------------ online_2D_serialmodel ESTKF ---------------"
 setenv OMP_NUM_THREADS 1
 cd online_2D_serialmodel
@@ -148,35 +50,3 @@ mpirun --oversubscribe -np 9 ./model_pdaf -dim_ens 9 $DA_SPECS2 > ../out.online_
 cd ..
 python verification/check_online2.py online_2D_serialmodel online_2D_serialmodel_ESTKF
 
-echo "------------ online_2D_serialmodel_2fields ESTKF ---------------"
-setenv OMP_NUM_THREADS 1
-cd online_2D_serialmodel_2fields
-make cleandata
-mpirun --oversubscribe -np 9 ./model_pdaf -dim_ens 9 $DA_SPECS2 > ../out.online_2D_serialmodel_2fields_ESTKF
-cd ..
-python verification/check_online2.py online_2D_serialmodel_2fields online_2D_serialmodel_ESTKF
-
-echo "------------ online_2D_parallelmodel ESTKF ---------------"
-setenv OMP_NUM_THREADS 1
-cd online_2D_parallelmodel
-make cleandata
-mpirun --oversubscribe -np 18 ./model_pdaf -dim_ens 9 $DA_SPECS2 > ../out.online_2D_parallelmodel_ESTKF
-cd ..
-python verification/check_online2.py online_2D_parallelmodel online_2D_parallelmodel_ESTKF
-
-
-echo "------------ online_2D_parallelmodel_fullpar ---------------"
-setenv OMP_NUM_THREADS 1
-cd online_2D_parallelmodel_fullpar
-make cleandata
-mpirun --oversubscribe -np 20 ./model_pdaf -dim_ens 9 $DA_SPECS > ../out.online_2D_parallelmodel_fullpar
-cd ..
-python verification/check_online.py online_2D_parallelmodel_fullpar
-
-echo "------------ online_2D_parallelmodel_fullpar_1fpe ---------------"
-setenv OMP_NUM_THREADS 1
-cd online_2D_parallelmodel_fullpar_1fpe
-make cleandata
-mpirun --oversubscribe -np 19 ./model_pdaf -dim_ens 9 $DA_SPECS > ../out.online_2D_parallelmodel_fullpar_1fpe
-cd ..
-python verification/check_online.py online_2D_parallelmodel_fullpar_1fpe
