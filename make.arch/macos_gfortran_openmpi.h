a85f2d49970f969b3b61c4f4a99dc2aba72c03a3
Lars Nerger <94826174+larsnerger@users.noreply.github.com>
2021-03-29 13:38:22 +0000

    Removing GCC10 -fallow-argument-mismatch

--- make.arch/macos_gfortran_openmpi.h
+++ make.arch/macos_gfortran_openmpi.h
@@ -33,9 +33,10 @@ CPP_DEFS = -DUSE_PDAF
 
 # Optimization specs for compiler
 # To use OpenMP parallelization in PDAF, specify it here (-fopenmp (gfortran) or -openmp (ifort))
+# For gfortran 10 and later, you probably need to set -fallow-argument-mismatch
 #   (You should explicitly define double precision for floating point
 #   variables in the compilation)  
-OPT = -O3 -fdefault-real-8 -fallow-argument-mismatch -Wall #-fopenmp #-fcheck=all
+OPT = -O3 -fdefault-real-8 -fallow-argument-mismatch
 
 # Optimization specifications for Linker
 OPT_LNK = $(OPT)
