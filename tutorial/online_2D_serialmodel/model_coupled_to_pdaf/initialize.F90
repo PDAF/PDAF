cd8751eec82e08e66e2eb498f564e1cf1d956526
Lars Nerger <94826174+larsnerger@users.noreply.github.com>
2021-03-30 17:06:01 +0000

    Moving full code with PDAF into model_coupled_to_pdaf

--- tutorial/online_2D_serialmodel/model_coupled_to_pdaf/initialize.F90
+++ tutorial/online_2D_serialmodel/model_coupled_to_pdaf/initialize.F90
@@ -44,7 +44,7 @@ SUBROUTINE initialize()
 ! *** Read initial field from file ***
 ! ************************************
 
-  OPEN(11, file = '../inputs_online/true_initial.txt', status='old')
+  OPEN(11, file = '../../inputs_online/true_initial.txt', status='old')
  
   DO i = 1, ny
      READ (11, *) field(i, :)
