27a0746ccd289e913bdce09b9da2ac392d055e52
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2021-11-07 21:07:19 +0000

    Adding Lorenz05 model III (lorenz05c) in implementation with OMI

---
+++ trunk/models/lorenz05c_omi/mod_model.F90
@@ -0,0 +1,34 @@
+!$Id: mod_model.F90 61 2019-02-01 08:49:36Z lnerger $
+!BOP
+!
+! !MODULE:
+MODULE mod_model
+
+! !DESCRIPTION:
+! This module provides shared variables for the Lorenz05c model.
+!
+! !REVISION HISTORY:
+! 2004-11 - Lars Nerger - Initial code
+! Later revisions - see svn log
+!
+! !USES:
+  IMPLICIT NONE
+  SAVE
+
+! !PUBLIC DATA MEMBERS:
+!    ! Control model run - available as command line options
+  INTEGER :: dim_state           ! Model state dimension
+  INTEGER :: step_null           ! Initial time stepforcing of assimilation
+
+!    ! Other variables - _NOT_ available as command line options!
+  INTEGER   :: step_final          ! Final time step
+  INTEGER   :: k_avg
+  REAL      :: fluctuate_coef
+  REAL      :: coupling_coef
+  INTEGER   :: smoothing_scale
+  REAL      :: dt                  ! Time step size
+  REAL, ALLOCATABLE :: x(:)      ! Array holding model field
+  REAL      :: forcing             ! Model parameter
+!EOP
+
+END MODULE mod_model
