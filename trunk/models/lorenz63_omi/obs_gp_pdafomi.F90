8896d0d506b7e2089af6a2658da04fbf9231ad2a
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2021-11-13 16:33:37 +0000

    Clean up

--- trunk/models/lorenz63_omi/obs_gp_pdafomi.F90
+++ trunk/models/lorenz63_omi/obs_gp_pdafomi.F90
@@ -284,7 +284,6 @@ CONTAINS
           s = 1
           stat(s) = NF90_INQ_VARID(fileid, 'obs', id_obs)
 
-write (*,*) 'read obs:', step, delt_obs_file
           WRITE (*,'(8x,a,i6)') &
                '--- Read observation at file position', step / delt_obs_file
 
