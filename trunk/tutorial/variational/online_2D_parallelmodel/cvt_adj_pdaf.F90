2dfd11a85361bfde422f1eace69a450698ccb0fc
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2021-11-29 18:25:31 +0000

    Copy VAR implmentation of online_2D_parallelmodel to trunk

---
+++ trunk/tutorial/variational/online_2D_parallelmodel/cvt_adj_pdaf.F90
@@ -0,0 +1,95 @@
+!$Id: obs_op_pdaf.F90 1864 2017-12-20 19:53:30Z lnerger $
+!BOP
+!
+! !ROUTINE: cvt_adj_pdaf --- Apply adjoint covariance operator
+!
+! !INTERFACE:
+SUBROUTINE cvt_adj_pdaf(iter, dim_p, dim_cv_p, Vcv_p, cv_p)
+
+! !DESCRIPTION:
+! User-supplied routine for PDAF.
+! Used in: 3D-Var and hybrid 3D-Var
+!
+! The routine is called during the analysis step.
+! It has to apply the adjoint covariance operator 
+! (transpose of square root of P) to a vector in
+! state space.
+!
+! For domain decomposition, the action is for
+! the PE-local sub-domain of the state. Thus the
+! covariance operator is applied to a sub-state.
+!
+! This code variant uses an explicit array holding
+! the covariance operator as a matrix.
+!
+! !REVISION HISTORY:
+! 2021-03 - Lars Nerger - Initial code
+! Later revisions - see svn log
+!
+! !USES:
+  USE mod_assimilation, &
+       ONLY: Vmat_p, dim_cvec, off_cv_p, type_opt
+  USE mod_parallel_pdaf, &
+       ONLY: MPI_REAL8, COMM_filter, MPI_SUM, MPIerr, mype_filter
+
+  IMPLICIT NONE
+
+! !ARGUMENTS:
+  INTEGER, INTENT(in) :: iter           ! Iteration of optimization
+  INTEGER, INTENT(in) :: dim_p          ! PE-local observation dimension
+  INTEGER, INTENT(in) :: dim_cv_p       ! PE-local dimension of control vector
+  REAL, INTENT(in)    :: Vcv_p(dim_p)   ! PE-local input vector
+  REAL, INTENT(inout) :: cv_p(dim_cvec) ! PE-local result vector
+!EOP
+
+! *** local variables ***
+  INTEGER :: i                        ! Counters
+  REAL, ALLOCATABLE :: cv_g(:)        ! Global control vector
+  REAL, ALLOCATABLE :: cv_g_part(:)   ! Global control vector (partial sums)
+
+
+! *****************************************************
+! *** Compute Vmat^T x_p with x_p some state vector ***
+! *****************************************************
+
+  ALLOCATE(cv_g_part(dim_cvec))
+
+  IF (type_opt==12 .OR. type_opt==13) THEN
+
+     ! Initialize distributed vector on control space
+     ALLOCATE(cv_g(dim_cvec))
+
+     ! Transform control variable to state increment 
+     ! - global vector of partial sums
+     CALL dgemv('t', dim_p, dim_cvec, 1.0, Vmat_p, &
+          dim_p, Vcv_p, 1, 0.0, cv_g_part, 1)
+
+     ! Get global vector with global sums
+     CALL MPI_Allreduce(cv_g_part, cv_g, dim_cvec, MPI_REAL8, MPI_SUM, &
+          COMM_filter, MPIerr)
+     
+     ! Select PE-local part of control vector
+     DO i = 1, dim_cv_p
+        cv_p(i) = cv_g(i + off_cv_p(mype_filter+1))
+     END DO
+
+     DEALLOCATE(cv_g)
+
+  ELSE
+
+     ! Transform control variable to state increment
+     CALL dgemv('t', dim_p, dim_cv_p, 1.0, Vmat_p, &
+          dim_p, Vcv_p, 1, 0.0, cv_g_part, 1)
+
+     ! Get global vector with global sums
+     CALL MPI_Allreduce(cv_g_part, cv_p, dim_cvec, MPI_REAL8, MPI_SUM, &
+          COMM_filter, MPIerr)
+
+  END IF
+
+
+! *** Clean up ***
+
+  DEALLOCATE(cv_g_part)
+
+END SUBROUTINE cvt_adj_pdaf
---
+++ trunk/tutorial/variational/online_2D_parallelmodel/init_3dvar_pdaf.F90
@@ -0,0 +1,147 @@
+!$Id: init_3dvar.F90 1861 2017-12-19 07:38:48Z lnerger $
+!BOP
+!
+! !ROUTINE: init_3dvar_pdaf --- Initialize ensemble
+!
+! !INTERFACE:
+SUBROUTINE init_3dvar_pdaf(filtertype, dim_p, dim_ens, state_p, Uinv, &
+     ens_p, flag)
+
+! !DESCRIPTION:
+! User-supplied routine for PDAF.
+! Used in the filters: SEIK/LSEIK/ETKF/LETKF/ESTKF/LESTKF
+!
+! The routine is called when the filter is
+! initialized in PDAF\_filter\_init.  It has
+! to initialize an ensemble of dim\_ens states.
+! Typically, the ensemble will be directly read from files.
+!
+! The routine is called by all filter processes and 
+! initializes the ensemble for the PE-local domain.
+!
+! Implementation for the 2D online example
+! without parallelization.
+!
+! !REVISION HISTORY:
+! 2013-02 - Lars Nerger - Initial code based on offline_1D
+! Later revisions - see svn log
+!
+! !USES:
+  USE mod_model, &
+       ONLY: nx, ny, nx_p
+  USE mod_parallel_model, &
+       ONLY: mype_model
+  USE mod_parallel_pdaf, &
+       ONLY: mype_filter
+  USE mod_assimilation, &
+       ONLY: dim_cvec, Vmat_p
+
+  IMPLICIT NONE
+
+! !ARGUMENTS:
+  INTEGER, INTENT(in) :: filtertype              ! Type of filter to initialize
+  INTEGER, INTENT(in) :: dim_p                   ! PE-local state dimension
+  INTEGER, INTENT(in) :: dim_ens                 ! Size of ensemble
+  REAL, INTENT(inout) :: state_p(dim_p)          ! PE-local model state
+  ! It is not necessary to initialize the array 'state_p' for 3D-Var
+  ! It is available here only for convenience and can be used freely.
+  REAL, INTENT(inout) :: Uinv(1,1)               ! Array not referenced for 3D-Var
+  REAL, INTENT(out)   :: ens_p(dim_p, dim_ens)   ! PE-local state ensemble
+  INTEGER, INTENT(inout) :: flag                 ! PDAF status flag
+
+! !CALLING SEQUENCE:
+! Called by: PDAF_filter_init    (as U_init_ens)
+!EOP
+
+! *** local variables ***
+  INTEGER :: i, j, member             ! Counters
+  REAL, ALLOCATABLE :: field(:,:)     ! global model field
+  CHARACTER(len=2) :: ensstr          ! String for ensemble member
+  REAL :: invdim_ens                  ! Inverse ensemble size
+  REAL :: fact                        ! Scaling factor
+
+
+! **********************
+! *** INITIALIZATION ***
+! **********************
+
+  ! *** Generate full ensemble on filter-PE 0 ***
+  IF (mype_filter==0) THEN
+     WRITE (*, '(/9x, a)') 'Initialize state and B^1/2 for 3D-Var'
+     WRITE (*, '(9x, a)') '--- read ensemble from files'
+     WRITE (*, '(9x, a, i5)') '--- members in B^1/2:  ', dim_cvec
+  END IF
+
+  ! Initialize numbers 
+  invdim_ens = 1.0 / REAL(dim_cvec)
+
+  ! allocate memory for temporary fields
+  ALLOCATE(field(ny, nx))
+
+  ! Allocate matrix holding B^1/2 (from mod_assimilation)
+  ALLOCATE(Vmat_p(dim_p, dim_cvec))
+
+
+! ********************************
+! *** Read ensemble from files ***
+! ********************************
+
+  DO member = 1, dim_cvec
+     WRITE (ensstr, '(i1)') member
+     OPEN(11, file = '../inputs_online/ens_'//TRIM(ensstr)//'.txt', status='old')
+
+     ! Read global field
+     DO i = 1, ny
+        READ (11, *) field(i, :)
+     END DO
+
+     ! Initialize process-local part of ensemble
+     DO j = 1, nx_p
+        Vmat_p(1 + (j-1)*ny : j*ny, member) = field(1:ny, nx_p*mype_model + j)
+     END DO
+
+     CLOSE(11)
+  END DO
+
+
+! **********************************************
+! *** Initialize square-root of P for 3D-Var ***
+! **********************************************
+
+  WRITE (*, '(9x, a)') 'Initialize B^1/2'
+
+  ! Here, we simply use the scaled ensemble perturbations
+
+  ! Compute ensemble mean
+  state_p = 0.0
+  DO member = 1, dim_cvec
+     DO i = 1, dim_p
+        state_p(i) = state_p(i) + Vmat_p(i, member)
+     END DO
+  END DO
+  state_p(:) = invdim_ens * state_p(:)
+
+  ! Initialize ensemble perturbations
+  DO member = 1, dim_cvec
+     Vmat_p(:,member) = Vmat_p(:,member) - state_p(:)
+  END DO
+
+  fact = 1.0/SQRT(REAL(dim_cvec-1))
+
+  Vmat_p = Vmat_p * fact
+
+
+! ******************************************
+! *** Initialize ensemble array for PDAF ***
+! ******************************************
+
+  ens_p(:,1) = state_p(:)
+
+
+! ****************
+! *** clean up ***
+! ****************
+
+  DEALLOCATE(field)
+
+END SUBROUTINE init_3dvar_pdaf
