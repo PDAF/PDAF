de93cce628a6c809e85c1f7350a2e648b9a6d784
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2021-12-07 14:59:13 +0000

    Correct typo

--- trunk/src/PDAF_etkf_analysis_fixed.F90
+++ trunk/src/PDAF_etkf_analysis_fixed.F90
@@ -134,7 +134,7 @@ SUBROUTINE PDAF_etkf_analysis_fixed(step, dim_p, dim_obs_p, dim_ens, &
 
   IF (mype == 0 .AND. screen > 0) THEN
      WRITE (*, '(a, i7, 3x, a)') &
-          'PDAF ', step, 'Assimilating observations - ESTKF with fixed ensemble'
+          'PDAF ', step, 'Assimilating observations - ETKF with fixed ensemble'
   END IF
 
 
