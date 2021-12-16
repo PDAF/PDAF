062522fc3a65f8811f31f522708824d781a93a02
Lars Nerger <94826174+larsnerger@users.noreply.github.com>
2021-12-16 15:51:21 +0000

    Update from syncing with PDAF V2.0

--- tutorial/variational/offline_2D_serial/cvt_pdaf.F90
+++ tutorial/variational/offline_2D_serial/cvt_pdaf.F90
@@ -1,6 +1,10 @@
 !$Id$
 !> Apply covariance operator to a control vector
 !!
+!! User-supplied call-back routine for PDAF.
+!!
+!! Used in 3D-Var and Hybrid 3D-Var.
+!!
 !! The routine is called during the analysis step of
 !! 3D-Var or hybrid 3D-Var. It has to apply the 
 !! covariance operator (square root of P) to a vector 
@@ -9,6 +13,8 @@
 !! For domain decomposition, the action is on
 !! the control vector for the PE-local part of
 !! the sub-state vector for the PE-local domain.
+!! In addition the control vector can also be 
+!! distributed (in case of type_opt=12 or 13).
 !!
 !! This code variant uses an explicit array holding
 !! the covariance operator as a matrix.
@@ -28,13 +34,14 @@ SUBROUTINE cvt_pdaf(iter, dim_p, dim_cvec, v_p, Vv_p)
   INTEGER, INTENT(in) :: iter          !< Iteration of optimization
   INTEGER, INTENT(in) :: dim_p         !< PE-local observation dimension
   INTEGER, INTENT(in) :: dim_cvec      !< Dimension of control vector
-  REAL, INTENT(in)    :: v_p(dim_cvec) !< PE-local model state
+  REAL, INTENT(in)    :: v_p(dim_cvec) !< PE-local control vector
   REAL, INTENT(inout) :: Vv_p(dim_p)   !< PE-local result vector
 
 
-! ************************
-! *** Compute Vmat v_p ***
-! ************************
+! ***************************************************
+! *** Apply covariance operator to control vector ***
+! *** by computing Vmat v_p                       ***
+! ***************************************************
 
   ! Transform control variable to state increment
   CALL dgemv('n', dim_p, dim_cvec, 1.0, Vmat_p, &
--- tutorial/variational/online_2D_parallelmodel/cvt_adj_pdaf.F90
+++ tutorial/variational/online_2D_parallelmodel/cvt_adj_pdaf.F90
@@ -1,18 +1,20 @@
 !$Id: obs_op_pdaf.F90 1864 2017-12-20 19:53:30Z lnerger $
 !> Apply adjoint covariance operator to a state vector
 !!
-!! The routine is called during the analysis step.
-!! It has to apply the adjoint covariance operator 
-!! (transpose of square root of P) to a vector in
-!! state space.
+!! User-supplied call-back routine for PDAF.
+!!
+!! Used in 3D-Var and Hybrid 3D-Var.
+!!
+!! The routine is called during the analysis step of
+!! 3D-Var or hybrid 3D-Var. It has to apply the 
+!! adjoint covariance operator (transpose of square
+!! root of B) to a vector in state space.
 !!
 !! For domain decomposition, the action is for
 !! the PE-local sub-domain of the state. Thus the
 !! covariance operator is applied to a sub-state.
-!!
-!! For domain decomposition, the action is for
-!! the PE-local sub-domain of the state. Thus the
-!! covariance operator is applied to a sub-state.
+!! In addition the control vector can also be 
+!! distributed (in case of type_opt=12 or 13).
 !!
 !! This code variant uses an explicit array holding
 !! the covariance operator as a matrix.
@@ -21,68 +23,69 @@
 !! * 2021-03 - Lars Nerger - Initial code
 !! * Later revisions - see repository log
 !!
-SUBROUTINE cvt_adj_pdaf(iter, dim_p, dim_cv_p, Vcv_p, cv_p)
+SUBROUTINE cvt_adj_pdaf(iter, dim_p, dim_cvec_p, Vv_p, v_p)
 
   USE MPI                     ! MPI
   USE mod_assimilation, &     ! Assimilation variables
-       ONLY: Vmat_p, dim_cvec, off_cv_p, type_opt
+       ONLY: Vmat_p, dim_cvec, off_v_p, type_opt
   USE mod_parallel_pdaf, &    ! PDAF parallelization variables
        ONLY: COMM_filter, MPIerr, mype_filter
 
   IMPLICIT NONE
 
 ! *** Arguments ***
-  INTEGER, INTENT(in) :: iter           !< Iteration of optimization
-  INTEGER, INTENT(in) :: dim_p          !< PE-local observation dimension
-  INTEGER, INTENT(in) :: dim_cv_p       !< Dimension of control vector
-  REAL, INTENT(in)    :: Vcv_p(dim_p)   !< PE-local input vector
-  REAL, INTENT(inout) :: cv_p(dim_cv_p) !< PE-local result vector
+  INTEGER, INTENT(in) :: iter            !< Iteration of optimization
+  INTEGER, INTENT(in) :: dim_p           !< PE-local observation dimension
+  INTEGER, INTENT(in) :: dim_cvec_p      !< Dimension of control vector
+  REAL, INTENT(in)    :: Vv_p(dim_p)     !< PE-local input vector
+  REAL, INTENT(inout) :: v_p(dim_cvec_p) !< PE-local result vector
 
 ! *** local variables ***
-  INTEGER :: i                        ! Counters
-  REAL, ALLOCATABLE :: cv_g(:)        ! Global control vector
-  REAL, ALLOCATABLE :: cv_g_part(:)   ! Global control vector (partial sums)
+  INTEGER :: i                        ! Counter
+  REAL, ALLOCATABLE :: v_g(:)         ! Global control vector
+  REAL, ALLOCATABLE :: v_g_part(:)    ! Global control vector (partial sums)
 
 
-! *****************************************************
-! *** Compute Vmat^T x_p with x_p some state vector ***
-! *****************************************************
+! ***************************************************
+! *** Apply covariance operator to a state vector ***
+! *** by computing Vmat^T Vv_p                    ***
+! ***************************************************
 
-  ALLOCATE(cv_g_part(dim_cvec))
+  ALLOCATE(v_g_part(dim_cvec))
 
   IF (type_opt==12 .OR. type_opt==13) THEN
 
      ! For the domain-decomposed solvers
 
      ! Initialize distributed vector on control space
-     ALLOCATE(cv_g(dim_cvec))
+     ALLOCATE(v_g(dim_cvec))
 
      ! Transform control variable to state increment 
      ! - global vector of partial sums
      CALL dgemv('t', dim_p, dim_cvec, 1.0, Vmat_p, &
-          dim_p, Vcv_p, 1, 0.0, cv_g_part, 1)
+          dim_p, Vv_p, 1, 0.0, v_g_part, 1)
 
      ! Get global vector with global sums
-     CALL MPI_Allreduce(cv_g_part, cv_g, dim_cvec, MPI_REAL8, MPI_SUM, &
+     CALL MPI_Allreduce(v_g_part, v_g, dim_cvec, MPI_REAL8, MPI_SUM, &
           COMM_filter, MPIerr)
      
      ! Select PE-local part of control vector
-     DO i = 1, dim_cv_p
-        cv_p(i) = cv_g(i + off_cv_p(mype_filter+1))
+     DO i = 1, dim_cvec_p
+        v_p(i) = v_g(i + off_v_p(mype_filter+1))
      END DO
 
-     DEALLOCATE(cv_g)
+     DEALLOCATE(v_g)
 
   ELSE
 
      ! Without domain-decomposition
 
      ! Transform control variable to state increment
-     CALL dgemv('t', dim_p, dim_cv_p, 1.0, Vmat_p, &
-          dim_p, Vcv_p, 1, 0.0, cv_g_part, 1)
+     CALL dgemv('t', dim_p, dim_cvec_p, 1.0, Vmat_p, &
+          dim_p, Vv_p, 1, 0.0, v_g_part, 1)
 
      ! Get global vector with global sums
-     CALL MPI_Allreduce(cv_g_part, cv_p, dim_cvec, MPI_REAL8, MPI_SUM, &
+     CALL MPI_Allreduce(v_g_part, v_p, dim_cvec, MPI_REAL8, MPI_SUM, &
           COMM_filter, MPIerr)
 
   END IF
@@ -90,6 +93,6 @@ SUBROUTINE cvt_adj_pdaf(iter, dim_p, dim_cv_p, Vcv_p, cv_p)
 
 ! *** Clean up ***
 
-  DEALLOCATE(cv_g_part)
+  DEALLOCATE(v_g_part)
 
 END SUBROUTINE cvt_adj_pdaf
