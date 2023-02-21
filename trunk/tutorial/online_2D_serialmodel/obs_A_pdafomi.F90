589fdb2c6fc35ed9f249db2eebddf8ef97815820
lnerger <lnerger@03dc7b85-0ec3-42b9-bb47-f5c5bde9c310>
2023-02-21 10:25:48 +0000

    Remove activation of OMI debug output

--- trunk/tutorial/online_2D_serialmodel/obs_A_pdafomi.F90
+++ trunk/tutorial/online_2D_serialmodel/obs_A_pdafomi.F90
@@ -368,7 +368,7 @@ CONTAINS
   SUBROUTINE init_dim_obs_l_A(domain_p, step, dim_obs, dim_obs_l)
 
     ! Include PDAFomi function
-    USE PDAFomi, ONLY: PDAFomi_init_dim_obs_l, PDAFomi_set_debug_flag
+    USE PDAFomi, ONLY: PDAFomi_init_dim_obs_l
 
     ! Include localization radius and local coordinates
     USE mod_assimilation, &   
@@ -381,11 +381,7 @@ CONTAINS
     INTEGER, INTENT(in)  :: step         !< Current time step
     INTEGER, INTENT(in)  :: dim_obs      !< Full dimension of observation vector
     INTEGER, INTENT(inout) :: dim_obs_l  !< Local dimension of observation vector
-if (domain_p==621) then
-   CALL PDAFomi_set_debug_flag(domain_p)
-else
-   CALL PDAFomi_set_debug_flag(0)
-end if
+
 ! **********************************************
 ! *** Initialize local observation dimension ***
 ! **********************************************
