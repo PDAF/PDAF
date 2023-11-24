c90bfa5ebb68a1fc4ad3d5cd1d5b5a24d685d86c
Johannes Keller <jo.keller@fz-juelich.de>
2023-11-24 14:11:56 +0100

    make.arch: introduce cmake.h

---
+++ make.arch/cmake.h
@@ -0,0 +1,54 @@
+######################################################
+# Include file with machine-specific definitions     #
+# for building PDAF.                                 #
+#                                                    #
+# Variant for CMake with eTSMP                       #
+#                                                    #
+######################################################
+# $Id: linux_ifort.h 1395 2013-05-03 13:44:37Z lnerger $
+
+# Compiler, Linker, and Archiver
+# FC = ${FC} # Using environment default
+LD = $(FC)
+# CC = ${CC} # Using environment default
+AR = ar
+RANLIB = ranlib 
+
+# C preprocessor
+# (only required, if preprocessing is not performed via the compiler)
+CPP = 
+
+# Definitions for CPP
+# Define USE_PDAF to include PDAF
+# Define BLOCKING_MPI_EXCHANGE to use blocking MPI commands to exchange data between model and PDAF
+# (if the compiler does not support get_command_argument()
+# from Fortran 2003 you should define F77 here.)
+CPP_DEFS = -DUSE_PDAF
+
+# Optimization specs for compiler
+#   (You should explicitly define double precision for floating point
+#   variables in the compilation)  
+OPT= ${TSMPPDAFOPTIM}
+
+# Optimization specifications for Linker
+OPT_LNK = $(OPT)
+
+# Linking libraries (BLAS, LAPACK, if required: MPI)
+LINK_LIBS = ${TSMPPDAFLINK_LIBS}
+
+
+# Specifications for the archiver
+AR_SPEC = 
+
+# Specifications for ranlib
+RAN_SPEC =
+
+# Include path for MPI header file
+MPI_INC = ${TSMPPDAFMPI_INC}
+
+# Object for nullMPI - if compiled without MPI library
+#OBJ_MPI = nullmpi.o
+
+# NetCDF (only required for Lorenz96)
+NC_LIB   = 
+NC_INC   = 
