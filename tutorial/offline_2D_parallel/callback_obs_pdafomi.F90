ae3058ac022e145df421d225a4dd43d5a6720346
Lars Nerger <lars.nerger@awi.de>
2024-02-23 16:17:47 +0100

    Correction of coordinate computation in case of parallelization

--- tutorial/offline_2D_parallel/callback_obs_pdafomi.F90
+++ tutorial/offline_2D_parallel/callback_obs_pdafomi.F90
@@ -146,22 +146,27 @@ SUBROUTINE localize_covar_pdafomi(dim_p, dim_obs, HP_p, HPH)
 
   ! Include functions for different observations
   USE obs_A_pdafomi, ONLY: localize_covar_A
+  USE obs_B_pdafomi, ONLY: localize_covar_B
 
   ! Include information on model grid
-  USE mod_assimilation, &
-       ONLY: nx, ny
+  USE mod_assimilation, &      ! Variables for assimilation
+       ONLY: ny, local_dims
+  ! Include parallelization information
+  USE mod_parallel_pdaf, &
+       ONLY: mype_filter
 
   IMPLICIT NONE
 
 ! *** Arguments ***
-  INTEGER, INTENT(in) :: dim_p                 !< PE-local state dimension
-  INTEGER, INTENT(in) :: dim_obs               !< number of observations
-  REAL, INTENT(inout) :: HP_p(dim_obs, dim_p)  !< PE local part of matrix HP
+  INTEGER, INTENT(in) :: dim_p                 !< Process-local state dimension
+  INTEGER, INTENT(in) :: dim_obs               !< Number of observations
+  REAL, INTENT(inout) :: HP_p(dim_obs, dim_p)  !< Process-local part of matrix HP
   REAL, INTENT(inout) :: HPH(dim_obs, dim_obs) !< Matrix HPH
 
 ! *** local variables ***
-  INTEGER :: i, j, cnt               ! Counters
-  REAL, ALLOCATABLE :: coords_p(:,:) ! Coordinates of PE-local state vector entries
+  INTEGER :: i, j                    ! Counters
+  INTEGER :: off_p                   ! Process-local offset in global state vector
+  REAL, ALLOCATABLE :: coords_p(:,:) ! Coordinates of process-local state vector entries
 
 
 ! **********************
@@ -172,13 +177,18 @@ SUBROUTINE localize_covar_pdafomi(dim_p, dim_obs, HP_p, HPH)
 
   ALLOCATE(coords_p(2, dim_p))
 
-  cnt = 0
-  DO j = 1, nx
-     DO i= 1, ny
-        cnt = cnt + 1
-        coords_p(1, cnt) = REAL(j)
-        coords_p(2, cnt) = REAL(i)
-     END DO
+  ! Global coordinates of local analysis domain
+  ! We use grid point indices as coordinates, but could e.g. use meters
+  ! The particular way to initializate coordinates is because in this
+  ! offline example we do not split the model domain, but the state vector.
+  off_p = 0
+  DO i = 1, mype_filter
+     off_p = off_p + local_dims(i)
+  END DO
+
+  DO i = 1, dim_p
+     coords_p(1, i) = REAL(CEILING(REAL(i+off_p)/REAL(ny)))
+     coords_p(2, i) = REAL(i+off_p) - (coords_p(1,i)-1)*REAL(ny)
   END DO
 
 
@@ -188,6 +198,7 @@ SUBROUTINE localize_covar_pdafomi(dim_p, dim_obs, HP_p, HPH)
 
   ! Call localize_covar specific for each observation
   CALL localize_covar_A(dim_p, dim_obs, HP_p, HPH, coords_p)
+  CALL localize_covar_B(dim_p, dim_obs, HP_p, HPH, coords_p)
 
 
 ! ****************
