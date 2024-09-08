f3aa6565fa800d55d3de763ffa15ccf07f9fa4a6
Lars Nerger <lars.nerger@awi.de>
2024-09-08 19:52:31 +0200

    Add example of user-provided routine to initialize local observations

--- tutorial/classical/offline_2D_parallel/finalize_pdaf.F90
+++ tutorial/classical/offline_2D_parallel/finalize_pdaf.F90
@@ -24,7 +24,7 @@ SUBROUTINE finalize_pdaf()
 !EOP
 
 ! *** Show allocated memory for PDAF ***
-  CALL PDAF_print_info(11)
+  IF (mype_world==0) CALL PDAF_print_info(10)
 
 ! *** Print PDAF timings onto screen ***
   IF (mype_world==0) CALL PDAF_print_info(3)
