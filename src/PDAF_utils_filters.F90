f681b99835b72f62ffdf11bdec380bcb429650df
Lars Nerger <lars.nerger@awi.de>
2025-02-15 12:31:14 +0100

    Update of dependencies

--- src/PDAF_utils_filters.F90
+++ src/PDAF_utils_filters.F90
@@ -21,15 +21,15 @@
 !! These routines are interfaces to the DA-method specific
 !! routines. The are called by PDAF_init, PDAF_print_info,
 !! PDAF_set_iparam or PDAF_set_rparam.
+!!
+!! !  These are core routines of PDAF and
+!!    should not be changed by the user   !
 !! 
 MODULE PDAF_utils_filters
 
 CONTAINS
 
-!> Interface routine to the filter-specific initialization routines.
-!!
-!! !  This is a core routine of PDAF and
-!!    should not be changed by the user   !
+!> Interface routine to the method-specific initialization routines.
 !!
 !! __Revision history:__
 !! * 2010-08 - Lars Nerger - Initial code for restructuring PDAF
@@ -216,10 +216,81 @@ CONTAINS
   END SUBROUTINE PDAF_init_filters
 
 !-------------------------------------------------------------------------------
-!> Print configuration info of the active filter
+!> Interface routine to the method-specific allocation routines.
 !!
-!! !  This is a core routine of PDAF and
-!!    should not be changed by the user   !
+!! __Revision history:__
+!! 2010-08 - Lars Nerger - Initial code from restructuring PDAF
+!! Other revisions - see repository log
+!!
+  SUBROUTINE PDAF_alloc_filters(filterstr, subtype, flag)
+
+    USE PDAF_seek, ONLY: PDAF_seek_alloc
+    USE PDAF_seik, ONLY: PDAF_seik_alloc
+    USE PDAF_lseik, ONLY: PDAF_lseik_alloc
+    USE PDAF_enkf, ONLY: PDAF_enkf_alloc
+    USE PDAF_etkf, ONLY: PDAF_etkf_alloc
+    USE PDAF_letkf, ONLY: PDAF_letkf_alloc
+    USE PDAF_estkf, ONLY: PDAF_estkf_alloc
+    USE PDAF_lestkf, ONLY: PDAF_lestkf_alloc
+    USE PDAF_lenkf, ONLY: PDAF_lenkf_alloc
+    USE PDAF_netf, ONLY: PDAF_netf_alloc
+    USE PDAF_lnetf, ONLY: PDAF_lnetf_alloc
+    USE PDAF_lknetf, ONLY: PDAF_lknetf_alloc
+    USE PDAF_pf, ONLY: PDAF_pf_alloc
+    USE PDAF_genobs, ONLY: PDAF_genobs_alloc
+    USE PDAF_3dvar, ONLY: PDAF_3dvar_alloc
+
+    IMPLICIT NONE
+
+! *** Arguments ***
+    CHARACTER(len=10), INTENT(in) :: filterstr ! Name of filter algorithm
+    INTEGER, INTENT(in) :: subtype             ! Sub-type of filter
+    INTEGER, INTENT(inout)::flag               ! Status flag
+
+
+! ***********************************************
+! *** Call filter-specific allocation routine ***
+! ***********************************************
+
+    checkflag: IF (flag == 0) THEN
+       IF (TRIM(filterstr) == 'SEEK') THEN
+          CALL PDAF_seek_alloc(flag)
+       ELSE IF (TRIM(filterstr) == 'SEIK') THEN
+          CALL PDAF_seik_alloc(flag)
+       ELSE IF (TRIM(filterstr) == 'ENKF') THEN
+          CALL PDAF_enkf_alloc(flag)
+       ELSE IF (TRIM(filterstr) == 'LSEIK') THEN
+          CALL PDAF_lseik_alloc(flag)
+       ELSE IF (TRIM(filterstr) == 'ETKF') THEN
+          CALL PDAF_etkf_alloc(flag)
+       ELSE IF (TRIM(filterstr) == 'LETKF') THEN
+          CALL PDAF_letkf_alloc(flag)
+       ELSE IF (TRIM(filterstr) == 'ESTKF') THEN
+          CALL PDAF_estkf_alloc(flag)
+       ELSE IF (TRIM(filterstr) == 'LESTKF') THEN
+          CALL PDAF_lestkf_alloc(flag)
+       ELSE IF (TRIM(filterstr) == 'LENKF') THEN
+          CALL PDAF_lenkf_alloc(flag)
+       ELSE IF (TRIM(filterstr) == 'NETF') THEN
+          CALL PDAF_netf_alloc(flag)
+       ELSE IF (TRIM(filterstr) == 'LNETF') THEN
+          CALL PDAF_lnetf_alloc(flag)
+       ELSE IF (TRIM(filterstr) == 'LKNETF') THEN
+          CALL PDAF_lknetf_alloc(flag)
+       ELSE IF (TRIM(filterstr) == 'PF') THEN
+          CALL PDAF_pf_alloc(flag)
+       ELSE IF (TRIM(filterstr) == 'GENOBS') THEN
+          CALL PDAF_genobs_alloc(flag)
+       ELSE IF (TRIM(filterstr) == '3DVAR') THEN
+          CALL PDAF_3dvar_alloc(subtype, flag)
+       ENDIF
+    END IF checkflag
+
+  END SUBROUTINE PDAF_alloc_filters
+
+
+!-------------------------------------------------------------------------------
+!> Print configuration info of the active filter
 !!
 !! __Revision history:__
 !! * 2025-02 - Lars Nerger - Initial code
@@ -292,10 +363,7 @@ CONTAINS
 !!
 !! This subroutine builds the interface for calling
 !! the screen output routine for the overview of
-!! options for the selected filter.
-!!
-!! !  This is a core routine of PDAF and
-!!    should not be changed by the user   !
+!! options for the selected DA method.
 !!
 !! __Revision history:__
 !! * 2011-08 - Lars Nerger - Initial code
@@ -374,82 +442,6 @@ CONTAINS
 
   END SUBROUTINE PDAF_options_filters
 
-!-------------------------------------------------------------------------------
-!> Interface routine to the filter-specific allocation routines.
-!!
-!! !  This is a core routine of PDAF and
-!!    should not be changed by the user   !
-!!
-!! __Revision history:__
-!! 2010-08 - Lars Nerger - Initial code from restructuring PDAF
-!! Other revisions - see repository log
-!!
-  SUBROUTINE PDAF_alloc_filters(filterstr, subtype, flag)
-
-    USE PDAF_seek, ONLY: PDAF_seek_alloc
-    USE PDAF_seik, ONLY: PDAF_seik_alloc
-    USE PDAF_lseik, ONLY: PDAF_lseik_alloc
-    USE PDAF_enkf, ONLY: PDAF_enkf_alloc
-    USE PDAF_etkf, ONLY: PDAF_etkf_alloc
-    USE PDAF_letkf, ONLY: PDAF_letkf_alloc
-    USE PDAF_estkf, ONLY: PDAF_estkf_alloc
-    USE PDAF_lestkf, ONLY: PDAF_lestkf_alloc
-    USE PDAF_lenkf, ONLY: PDAF_lenkf_alloc
-    USE PDAF_netf, ONLY: PDAF_netf_alloc
-    USE PDAF_lnetf, ONLY: PDAF_lnetf_alloc
-    USE PDAF_lknetf, ONLY: PDAF_lknetf_alloc
-    USE PDAF_pf, ONLY: PDAF_pf_alloc
-    USE PDAF_genobs, ONLY: PDAF_genobs_alloc
-    USE PDAF_3dvar, ONLY: PDAF_3dvar_alloc
-
-    IMPLICIT NONE
-
-! *** Arguments ***
-    CHARACTER(len=10), INTENT(in) :: filterstr ! Name of filter algorithm
-    INTEGER, INTENT(in) :: subtype             ! Sub-type of filter
-    INTEGER, INTENT(inout)::flag               ! Status flag
-
-
-! ***********************************************
-! *** Call filter-specific allocation routine ***
-! ***********************************************
-
-    checkflag: IF (flag == 0) THEN
-       IF (TRIM(filterstr) == 'SEEK') THEN
-          CALL PDAF_seek_alloc(flag)
-       ELSE IF (TRIM(filterstr) == 'SEIK') THEN
-          CALL PDAF_seik_alloc(flag)
-       ELSE IF (TRIM(filterstr) == 'ENKF') THEN
-          CALL PDAF_enkf_alloc(flag)
-       ELSE IF (TRIM(filterstr) == 'LSEIK') THEN
-          CALL PDAF_lseik_alloc(flag)
-       ELSE IF (TRIM(filterstr) == 'ETKF') THEN
-          CALL PDAF_etkf_alloc(flag)
-       ELSE IF (TRIM(filterstr) == 'LETKF') THEN
-          CALL PDAF_letkf_alloc(flag)
-       ELSE IF (TRIM(filterstr) == 'ESTKF') THEN
-          CALL PDAF_estkf_alloc(flag)
-       ELSE IF (TRIM(filterstr) == 'LESTKF') THEN
-          CALL PDAF_lestkf_alloc(flag)
-       ELSE IF (TRIM(filterstr) == 'LENKF') THEN
-          CALL PDAF_lenkf_alloc(flag)
-       ELSE IF (TRIM(filterstr) == 'NETF') THEN
-          CALL PDAF_netf_alloc(flag)
-       ELSE IF (TRIM(filterstr) == 'LNETF') THEN
-          CALL PDAF_lnetf_alloc(flag)
-       ELSE IF (TRIM(filterstr) == 'LKNETF') THEN
-          CALL PDAF_lknetf_alloc(flag)
-       ELSE IF (TRIM(filterstr) == 'PF') THEN
-          CALL PDAF_pf_alloc(flag)
-       ELSE IF (TRIM(filterstr) == 'GENOBS') THEN
-          CALL PDAF_genobs_alloc(flag)
-       ELSE IF (TRIM(filterstr) == '3DVAR') THEN
-          CALL PDAF_3dvar_alloc(subtype, flag)
-       ENDIF
-    END IF checkflag
-
-  END SUBROUTINE PDAF_alloc_filters
-
 
 !-------------------------------------------------------------------------------
 !> Print information for PDAF (timing and memory) to screen
@@ -457,9 +449,6 @@ CONTAINS
 !! This routine displays the information from PDAF.
 !! Possible are to display the timing information and
 !! allocated memory.
-!!
-!! !  This is a core routine of PDAF and
-!!    should not be changed by the user   !
 !!
 !! __Revision history:__
 !! * 2008-09 - Lars Nerger - Initial code
@@ -537,9 +526,6 @@ CONTAINS
 !! method-specific integer parameter. This routine
 !! builds the interface to the specific routine
 !! provided by each DA method.
-!!
-!!    ! This is a core routine of PDAF and !
-!!    ! should not be changed by the user  !
 !!
 !! __Revision history:__
 !! * 2025-02 - Lars Nerger - Initial code
@@ -631,9 +617,6 @@ CONTAINS
 !! method-specific real parameter. This routine
 !! builds the interface to the specific routine
 !! provided by each DA method.
-!!
-!!    ! This is a core routine of PDAF and !
-!!    ! should not be changed by the user  !
 !!
 !! __Revision history:__
 !! * 2025-02 - Lars Nerger - Initial code
