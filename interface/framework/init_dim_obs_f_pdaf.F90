7eb78cf55033214765bedbc1627463989faf218e
Johannes Keller <jo.keller@fz-juelich.de>
2025-07-18 09:58:19 +0200

    init_dim_obs_f_pdaf: only active for gridcells

--- interface/framework/init_dim_obs_f_pdaf.F90
+++ interface/framework/init_dim_obs_f_pdaf.F90
@@ -966,7 +966,16 @@ SUBROUTINE init_dim_obs_f_pdaf(step, dim_obs_f)
                    end if
 #endif
                  else
-                   obs_index_p(cnt) = g-begg+1 + ((endg-begg+1) * (clmobs_layer(i)-1))
+#ifdef CLMFIVE
+                   if(clmstatevec_only_active.eq.1) then
+                     obs_index_p(cnt) = state_clm2pdaf_p(c,clmobs_layer(i))
+                   else
+
+#endif
+                     obs_index_p(cnt) = g-begg+1 + ((endg-begg+1) * (clmobs_layer(i)-1))
+#ifdef CLMFIVE
+                   end if
+#endif
                  end if
 
                  !write(*,*) 'obs_index_p(',cnt,') is',obs_index_p(cnt)
