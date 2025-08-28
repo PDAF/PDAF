f2d841b2363b6c733d64b8df26d956435c2c4298
Johannes Keller <16795031+jjokella@users.noreply.github.com>
2025-08-28 13:02:37 +0200

    init_dim_obs: add discontinuity check for computing `deltax` (#15)

--- interface/framework/init_dim_obs_f_pdaf.F90
+++ interface/framework/init_dim_obs_f_pdaf.F90
@@ -473,6 +473,9 @@ SUBROUTINE init_dim_obs_f_pdaf(step, dim_obs_f)
         do g = begg, endg
             if(is_use_dr) then
                 deltax = abs(lon(g)-clmobs_lon(i))
+                if (deltax > 180.0) then
+                  deltax = 360.0 - deltax
+                end if
                 deltay = abs(lat(g)-clmobs_lat(i))
             end if
             ! Assigning observations to grid cells according to
@@ -627,6 +630,9 @@ SUBROUTINE init_dim_obs_f_pdaf(step, dim_obs_f)
 
               if(is_use_dr) then
                 deltax = abs(lon(g)-clmobs_lon(i))
+                if (deltax > 180.0) then
+                  deltax = 360.0 - deltax
+                end if
                 deltay = abs(lat(g)-clmobs_lat(i))
               end if
 
@@ -936,6 +942,9 @@ SUBROUTINE init_dim_obs_f_pdaf(step, dim_obs_f)
 
                if(is_use_dr) then
                  deltax = abs(lon(g)-clmobs_lon(i))
+                 if (deltax > 180.0) then
+                   deltax = 360.0 - deltax
+                 end if
                  deltay = abs(lat(g)-clmobs_lat(i))
                end if
 
--- interface/framework/init_dim_obs_pdaf.F90
+++ interface/framework/init_dim_obs_pdaf.F90
@@ -469,6 +469,9 @@ SUBROUTINE init_dim_obs_pdaf(step, dim_obs_p)
         do g = begg, endg
             if(is_use_dr) then
                 deltax = abs(lon(g)-clmobs_lon(i))
+                if (deltax > 180.0) then
+                  deltax = 360.0 - deltax
+                end if
                 deltay = abs(lat(g)-clmobs_lat(i))
             end if
             ! Assigning observations to grid cells according to
@@ -620,6 +623,9 @@ SUBROUTINE init_dim_obs_pdaf(step, dim_obs_p)
 
               if(is_use_dr) then
                 deltax = abs(lon(g)-clmobs_lon(i))
+                if (deltax > 180.0) then
+                  deltax = 360.0 - deltax
+                end if
                 deltay = abs(lat(g)-clmobs_lat(i))
               end if
 
@@ -929,6 +935,9 @@ SUBROUTINE init_dim_obs_pdaf(step, dim_obs_p)
 
                if(is_use_dr) then
                  deltax = abs(lon(g)-clmobs_lon(i))
+                 if (deltax > 180.0) then
+                   deltax = 360.0 - deltax
+                 end if
                  deltay = abs(lat(g)-clmobs_lat(i))
                end if
 
