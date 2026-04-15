a880e06501a5076f8ead1db05658466799d6fb53
Lars Nerger <lars.nerger@awi.de>
2026-04-15 17:52:58 +0200

    Revise mode_statevector. Remove init_pdaf_info

--- templates/online/prepoststep_pdaf.F90
+++ templates/online/prepoststep_pdaf.F90
@@ -35,7 +35,7 @@ SUBROUTINE prepoststep_pdaf(step, dim_p, dim_ens, dim_ens_p, dim_obs_p, &
   USE mod_assimilation, &      ! Assimilation variables
        ONLY: dim_state, do_omi_obsstats
   USE PDAF, &                  ! PDAF and PDAF-OMI diagnostic routines
-       ONLY: PDAF_diag_stddev, PDAFomi_diag_obs_rmsd, PDAFomi_diag_stats
+       ONLY: PDAF_diag_stddev, PDAFomi_diag_obs_rmsd, PDAFomi_diag_diffstats
 
   IMPLICIT NONE
 
@@ -112,7 +112,7 @@ SUBROUTINE prepoststep_pdaf(step, dim_p, dim_ens, dim_ens_p, dim_obs_p, &
      CALL PDAFomi_diag_obs_rmsd(nobs, obsrmsd, 1/(mype_filter+1))
 
      ! Compute statistics on deviation between observation and observed ensemble
-     CALL PDAFomi_diag_stats(nobs, obsstats, 1/(mype_filter+1))
+     CALL PDAFomi_diag_diffstats(nobs, obsstats, 1/(mype_filter+1))
   END IF
 
 
